5568bab7cafa9506e60717284666f37d752bcff9
yurem <Yuriy.Movchan@gmail.com>
2015-01-13 15:06:41 +0200

    Add sample interceptor script

---
+++ configuration/python/CacheRefreshInterceptor.py
@@ -0,0 +1,47 @@
+from org.xdi.model.custom.script.type.user import CacheRefreshType
+from org.xdi.util import StringHelper, ArrayHelper
+from java.util import Arrays, ArrayList
+from org.gluu.oxtrust.model import GluuCustomPerson
+from org.gluu.oxtrust.model import GluuCustomAttribute
+
+import java
+
+class CacheRefresh(CacheRefreshType):
+    def __init__(self, currentTimeMillis):
+        self.currentTimeMillis = currentTimeMillis
+
+    def init(self, configurationAttributes):
+        print "Cache refresh initialization"
+        print "Cache refresh initialized successfully"
+
+        return True   
+
+    def destroy(self, configurationAttributes):
+        print "Cache refresh destroy"
+        print "Cache refresh destroyed successfully"
+        return True   
+
+    # Update user entry before persist it
+    #   user is org.gluu.oxtrust.model.GluuCustomPerson
+    #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
+    def updateUser(self, user, configurationAttributes):
+        attributes = user.getCustomAttributes()
+
+        # Add new attribute preferredLanguage
+        attrPrefferedLanguage = GluuCustomAttribute("preferredLanguage", "en-us")
+        attributes.add(attrPrefferedLanguage)
+
+        # Add new attribute userPassword
+        attrUserPassword = GluuCustomAttribute("userPassword", "secret")
+        attributes.add(attrUserPassword)
+
+        # Update givenName attribute
+        for attribute in attributes:
+            attrName = attribute.getName()
+            if (("givenname" == StringHelper.toLowerCase(attrName)) and StringHelper.isNotEmpty(attribute.getValue())):
+                attribute.setValue(StringHelper.removeMultipleSpaces(attribute.getValue()) + " (updated)")
+
+        return True
+
+    def getApiVersion(self):
+        return 1
