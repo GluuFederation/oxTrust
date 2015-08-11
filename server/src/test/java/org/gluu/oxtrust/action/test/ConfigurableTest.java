48c967e7b99e55f6b3dcf1cb9574e0d9e38c5d33
yurem <Yuriy.Movchan@gmail.com>
2015-08-11 08:59:50 +0300

    Allow to put configuration file in specified location

--- server/src/test/java/org/gluu/oxtrust/action/test/ConfigurableTest.java
+++ server/src/test/java/org/gluu/oxtrust/action/test/ConfigurableTest.java
@@ -6,6 +6,8 @@
 
 package org.gluu.oxtrust.action.test;
 
+import java.io.File;
+
 import org.gluu.oxtrust.ldap.service.util.TestFileConfiguration;
 import org.jboss.seam.mock.JUnitSeamTest;
 import org.junit.Before;
@@ -27,7 +29,7 @@ public abstract class ConfigurableTest extends JUnitSeamTest {
 	@Before
 	public void initTest() {
 		super.begin();
-		this.testData = new TestFileConfiguration("oxTrustLdapTest.properties");
+		this.testData = new TestFileConfiguration(System.getProperty("catalina.home") + File.separator + "conf" + File.separator + "oxTrustLdapTest.properties");
 	}
 
 	/**
