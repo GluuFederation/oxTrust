b0787c39039c91ae535df22d7920545cc68832e4
shekhar16 <shekhar@gluu.org>
2016-07-04 21:03:53 +0530

    oxTrust issue #277

--- server/src/main/java/org/gluu/oxtrust/action/TrustRelationshipInventoryAction.java
+++ server/src/main/java/org/gluu/oxtrust/action/TrustRelationshipInventoryAction.java
@@ -12,6 +12,9 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.gluu.oxtrust.ldap.service.AttributeService;
 import org.gluu.oxtrust.ldap.service.TrustService;
 import org.gluu.oxtrust.model.GluuSAMLTrustRelationship;
@@ -24,6 +27,7 @@ import org.jboss.seam.annotations.security.Restrict;
 import org.jboss.seam.log.Log;
 import org.xdi.model.GluuAttribute;
 import org.xdi.model.GluuUserRole;
+import org.xdi.util.Util;
 
 /**
  * Action class for displaying trust relationships
@@ -48,6 +52,12 @@ public class TrustRelationshipInventoryAction implements Serializable {
 	@Logger
 	private Log log;
 
+	@NotNull
+	@Size(min = 0, max = 30, message = "Length of search string should be less than 30")
+	private String searchPattern;
+
+	private String oldSearchPattern;
+
 	private List<GluuSAMLTrustRelationship> trustedSpList;
 
 	public List<GluuSAMLTrustRelationship> getTrustedSpList() {
@@ -70,7 +80,13 @@ public class TrustRelationshipInventoryAction implements Serializable {
 	@Restrict("#{s:hasPermission('trust', 'access')}")
 	public String search() {
 		try {
-			this.trustedSpList = trustService.getAllTrustRelationships();
+			
+			if (Util.equals(this.oldSearchPattern, this.searchPattern)) {
+				return OxTrustConstants.RESULT_SUCCESS;
+			}
+			
+			this.trustedSpList = trustService.searchSAMLTrustRelationships(searchPattern,100);
+			this.oldSearchPattern = this.searchPattern;
 
 			setCustomAttributes(this.trustedSpList);
 		} catch (Exception ex) {
@@ -89,5 +105,21 @@ public class TrustRelationshipInventoryAction implements Serializable {
 			trustRelationship.setReleasedCustomAttributes(attributeService.getCustomAttributesByAttributeDNs(
 					trustRelationship.getReleasedAttributes(), attributesByDNs));
 		}
+	}
+
+	public String getSearchPattern() {
+		return searchPattern;
+	}
+
+	public void setSearchPattern(String searchPattern) {
+		this.searchPattern = searchPattern;
+	}
+
+	public String getOldSearchPattern() {
+		return oldSearchPattern;
+	}
+
+	public void setOldSearchPattern(String oldSearchPattern) {
+		this.oldSearchPattern = oldSearchPattern;
 	}
 }
