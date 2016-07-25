9effaea0b4f8686d88b924de485dde273f5d1dd0
Dmitry Ognyannikov <dmogn@mail.ru>
2016-07-25 17:22:45 -0300

    IDP configuration - Extract fields from uploaded metadata #283 (new class)

---
+++ server/src/main/java/org/gluu/oxtrust/util/MetadataUtil.java
@@ -0,0 +1,15 @@
+/*
+ * oxTrust is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2016, Gluu
+ */
+package org.gluu.oxtrust.util;
+
+/**
+ * Extract SAML Metadata fields.
+ *
+ * @author Dmitry Ognyannikov, 2016
+ */
+public class MetadataUtil {
+    
+}
