f687aa1f8735b98e65981e47aa3e3515337ff47e
shekhar16 <shekhar@gluu.org>
2016-07-30 22:56:02 +0530

    oxTrust issue 315

--- server/src/main/java/org/gluu/oxtrust/model/GluuSAMLTrustRelationship.java
+++ server/src/main/java/org/gluu/oxtrust/model/GluuSAMLTrustRelationship.java
@@ -109,7 +109,7 @@ public class GluuSAMLTrustRelationship extends InumEntry implements Serializable
 	@LdapAttribute(name = "url")
 	private String url;
 	
-	@Pattern(regexp="(^$)|(^(http|https)\\://[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,4}(:[a-zA-Z0-9]*)?/?$)", message = "Please enter a valid url, including protocol (http/https)")
+	@Pattern(regexp="^$|(^(http|https)\\://[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,4}(:[a-zA-Z0-9]*)?/?$)", message = "Please enter a valid url, including protocol (http/https)")
 	@LdapAttribute(name = "oxAuthPostLogoutRedirectURI")
 	private String spLogoutURL;
 
