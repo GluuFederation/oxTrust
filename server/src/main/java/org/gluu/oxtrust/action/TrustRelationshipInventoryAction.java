a65e0ac493e56b79482411aa731d9c4666c78b47
shekhar16 <shekhar@gluu.org>
2016-09-25 13:45:40 +0530

    oxTrust issue #354 : display all TRs on initial page.

--- server/src/main/java/org/gluu/oxtrust/action/TrustRelationshipInventoryAction.java
+++ server/src/main/java/org/gluu/oxtrust/action/TrustRelationshipInventoryAction.java
@@ -54,7 +54,7 @@ public class TrustRelationshipInventoryAction implements Serializable {
 
 	@NotNull
 	@Size(min = 0, max = 30, message = "Length of search string should be less than 30")
-	private String searchPattern;
+	private String searchPattern="";
 
 	private String oldSearchPattern;
 
