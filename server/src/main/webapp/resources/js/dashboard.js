ea92f47c7bde75a272b1eaf30ca28e2a6af37a42
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2017-01-19 20:29:24 +0200

    Remove deprecated options after updating chart js

--- server/src/main/webapp/resources/js/dashboard.js
+++ server/src/main/webapp/resources/js/dashboard.js
@@ -13,70 +13,26 @@ $(function () {
   console.log("authentication chart data");
   console.log(authChartData);
 
-  var authenticationRequestsChartData = {
-      labels: authChartData.labels,
-      datasets: [
-    
-{
-    label: "Successful Login",
-    fillColor: "#00BE79",
-    strokeColor: "#00BE79",
-    pointColor: "#00BE79",
-    pointStrokeColor: "#00BE79",
-    pointHighlightFill: "#fff",
-    pointHighlightStroke: "#00BE79",
-    data: authChartData.success
-},
-    {
-        label: "Failed Attempts",
-        fillColor: "#F39C12",
-        strokeColor: "#F39C12",
-        pointColor: "#F39C12",
-        pointStrokeColor: "#F39C12",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "#F39C12",
-        data: authChartData.failure
-    }
-    
-  ]
-  };
+	  var authenticationRequestsChartData = {
+		labels : authChartData.labels,
+		datasets : [
+	
+		{
+			label : "Successful Login",
+			fill : false,
+			backgroundColor : "#00BE79",
+			data : authChartData.success
+		}, {
+			label : "Failed Attempts",
+			fill : false,
+			backgroundColor : "#F39C12",
+			data : authChartData.failure
+		}
+	
+		]
+	};
 
   var authenticationRequestsChartOptions = {
-      //Boolean - If we should show the scale at all
-      showScale: true,
-      //Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines: false,
-      //String - Colour of the grid lines
-      scaleGridLineColor: "rgba(0,0,0,.05)",
-      //Number - Width of the grid lines
-      scaleGridLineWidth: 1,
-      //Boolean - Whether to show horizontal lines (except X axis)
-      scaleShowHorizontalLines: true,
-      //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines: true,
-      //Boolean - Whether the line is curved between points
-      bezierCurve: true,
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension: 0.3,
-      //Boolean - Whether to show a dot for each point
-      pointDot: false,
-      //Number - Radius of each point dot in pixels
-      pointDotRadius: 4,
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth: 1,
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius: 20,
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke: true,
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth: 2,
-      //Boolean - Whether to fill the dataset with a color
-      datasetFill: true,
-      //String - A legend template
-      legendTemplate: "<ul class=\"rahat\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%=datasets[i].label%></li><%}%></ul>",
-      //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-      maintainAspectRatio: false,
-      //Boolean - whether to make the chart responsive to window resizing
       responsive: true,
       scales: {
           yAxes: [{
