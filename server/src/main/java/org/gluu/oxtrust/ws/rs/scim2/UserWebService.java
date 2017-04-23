fa3f89da86a3cbc4d4ce67fbd224a751b1a56981
shekhar16 <shekhar@gluu.org>
2017-04-23 14:16:09 +0530

    oxTrust issue #360 allow search user without count vaue in request.

--- server/src/main/java/org/gluu/oxtrust/ws/rs/scim2/UserWebService.java
+++ server/src/main/java/org/gluu/oxtrust/ws/rs/scim2/UserWebService.java
@@ -69,7 +69,7 @@ public class UserWebService extends BaseScimWebService {
 		@QueryParam(OxTrustConstants.QUERY_PARAMETER_TEST_MODE_OAUTH2_TOKEN) final String token,
 		@QueryParam(OxTrustConstants.QUERY_PARAMETER_FILTER) final String filterString,
 		@QueryParam(OxTrustConstants.QUERY_PARAMETER_START_INDEX) final int startIndex,
-		@QueryParam(OxTrustConstants.QUERY_PARAMETER_COUNT) final int count,
+		@QueryParam(OxTrustConstants.QUERY_PARAMETER_COUNT)  Integer count,
 		@QueryParam(OxTrustConstants.QUERY_PARAMETER_SORT_BY) final String sortBy,
 		@QueryParam(OxTrustConstants.QUERY_PARAMETER_SORT_ORDER) final String sortOrder,
 		@QueryParam(OxTrustConstants.QUERY_PARAMETER_ATTRIBUTES) final String attributesArray) throws Exception {
@@ -86,6 +86,7 @@ public class UserWebService extends BaseScimWebService {
 		}
 
 		try {
+			count = (count == null) ? MAX_COUNT : count;
 
 			if (count > MAX_COUNT) {
 
