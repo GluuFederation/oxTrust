02dad9199f79defb469fc540bc4fa11e304205bd
yurem <Yuriy.Movchan@gmail.com>
2017-09-04 13:21:33 +0300

    Pass SP relayingParty to oxAuth

--- saml-openid-auth-client/src/main/java/org/gluu/oxauth/client/authentication/AuthenticationFilter.java
+++ saml-openid-auth-client/src/main/java/org/gluu/oxauth/client/authentication/AuthenticationFilter.java
@@ -159,7 +159,8 @@ public class AuthenticationFilter extends AbstractOAuthFilter {
 		String rfp = UUID.randomUUID().toString();
 		String jti = UUID.randomUUID().toString();
 		
-		String additionalClaims = "{relyingPartyId: 'relyingPartyId'}";
+		String relyingPartyId = edu.internet2.middleware.shibboleth.idp.util.HttpServletHelper.getLoginContext(request).getRelyingPartyId();
+		String additionalClaims = String.format("{relyingPartyId: '%s'}", relyingPartyId);
 
 		OxAuthCryptoProvider cryptoProvider = new OxAuthCryptoProvider();
 		JwtState jwtState = new JwtState(SignatureAlgorithm.HS256, clientSecret, cryptoProvider);
