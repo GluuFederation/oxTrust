e8cc8f5c7da3d93ee6d501885ce2cca8a273e3c3
Jose G <bonustrack310@gmail.com>
2017-10-02 08:41:47 -0500

    Move serialization stuff to separate class

--- server/src/main/java/org/gluu/oxtrust/ws/rs/scim2/BaseScimWebService.java
+++ server/src/main/java/org/gluu/oxtrust/ws/rs/scim2/BaseScimWebService.java
@@ -1,18 +1,13 @@
 package org.gluu.oxtrust.ws.rs.scim2;
 
-import org.codehaus.jackson.map.ObjectMapper;
 import org.gluu.oxtrust.model.scim2.ErrorResponse;
 import org.gluu.oxtrust.model.scim2.ErrorScimType;
-import org.gluu.oxtrust.model.scim2.BaseScimResource;
-import org.gluu.oxtrust.model.scim2.annotations.Schema;
-import org.gluu.oxtrust.model.scim2.util.IntrospectUtil;
+import org.gluu.oxtrust.service.scim2.serialization.ScimResourceSerializer;
 import org.slf4j.Logger;
+import org.xdi.config.oxtrust.AppConfiguration;
 
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
-import java.util.*;
-
-import static org.gluu.oxtrust.model.scim2.Constants.*;
 
 /**
  * Base methods for SCIM web services
@@ -25,177 +20,16 @@ public class BaseScimWebService {
     @Inject
     Logger log;
 
+    @Inject
+    AppConfiguration appConfiguration;
+
+    @Inject
+    ScimResourceSerializer resourceSerializer;
+
     String endpointUrl;
 
-    private ObjectMapper mapper=new ObjectMapper();
-
-    private Set<String> expandAttributesPaths(String attributes, String defaulSchemaUrn, List<String> schemas, SortedSet<String> attribs) {
-
-        Set<String> set=new HashSet<String>();
-
-        for (String attr : attributes.split(",")) {
-            String shorterName=attr.replaceAll("\\s", "");
-
-            for (String urn : schemas){
-                if (shorterName.startsWith(urn + ":")) {
-                    if (urn.equals(defaulSchemaUrn))
-                        shorterName = shorterName.substring(urn.length()+1);
-                    else
-                        shorterName = shorterName.substring(0, urn.length()) + "." + shorterName.substring(urn.length()+1);
-                }
-            }
-            set.add(shorterName);
-        }
-
-        Set<String> extendedSet=new HashSet<String>();
-
-        //attribs is already sorted
-        for (String basicAttr : set){
-            extendedSet.add(basicAttr);
-
-            for (String elem : attribs.tailSet(basicAttr + "."))
-                if (elem.startsWith(basicAttr + "."))
-                    extendedSet.add(elem);
-                else
-                    break;
-        }
-        //No need for this block: containsProperty method is smart enough
-        /*
-        //Remove redundancies
-        String attrArray[]=list.toArray(new String[]{});
-        Arrays.sort(attrArray);
-        list=new ArrayList<>();
-        int j=0;
-
-        for (int i=0; i<attrArray.length-1;){
-            String prevAttr=attrArray[i];
-            for (j=i+1;j<attrArray.length && attrArray[j].startsWith(prevAttr + ".");j++);
-            i=j;
-            list.add(prevAttr);
-        }
-           */
-        return extendedSet;
-    }
-
-    private void buildIncludeSet(SortedSet<String> include, Class<? extends BaseScimResource> resourceClass,
-                                 List<String> schemas, String attributes, String exclussions){
-
-        Set<String> tempSet;
-        Set<String> alwaysSet=IntrospectUtil.alwaysCoreAttrs.get(resourceClass).keySet();
-        Set<String> neverSet=IntrospectUtil.neverCoreAttrs.get(resourceClass).keySet();
-        Set<String> defaultSet=IntrospectUtil.defaultCoreAttrs.get(resourceClass).keySet();
-
-        String defaultSchema=resourceClass.getAnnotation(Schema.class).id();
-
-        if (attributes!=null) {
-            log.info("Processing attributes query param (excludedAttributes ignored)");
-
-            tempSet= expandAttributesPaths(attributes, defaultSchema, schemas, IntrospectUtil.allAttrs.get(resourceClass));
-            tempSet.removeAll(neverSet);
-            include.addAll(tempSet);
-        }
-        else
-        if (exclussions!=null){
-            log.info("Processing excludedAttributes query param)");
-
-            tempSet= defaultSet;
-            tempSet.removeAll(expandAttributesPaths(exclussions, defaultSchema, schemas, IntrospectUtil.allAttrs.get(resourceClass)));
-            include.addAll(tempSet);
-        }
-        else{
-            log.info("No attributes neither excludedAttributes query param were passed");
-            include.addAll(defaultSet);
-        }
-        include.addAll(alwaysSet);
-
-    }
-
-    private boolean containsProperty(SortedSet<String> properties, String prefix, String key){
-
-        String property = (prefix.length() == 0) ? key : prefix + "." + key;
-        Set<String> set=properties.tailSet(property);
-
-        boolean flag=set.contains(property);
-        if (!flag){
-            for (String prop : set)
-                if (prop.startsWith(property + ".")) {
-                    flag=true;
-                    break;
-                }
-        }
-        return flag;
-
-    }
-
-    private String getNewPrefix(String prefix, String key){
-        return prefix + (prefix.length()==0 ? "" : ".") + key;
-    }
-
-    private Map<String, Object> smallerMap(String prefix, Map<String, Object> value, SortedSet<String> include){
-        LinkedHashMap<String, Object> smallMap = new LinkedHashMap<String, Object>();
-        traverse(prefix, value, smallMap, include);
-        return smallMap.size()==0 ? null : smallMap;
-    }
-
-    /**
-     * Section 2.5 of RFC 7643: When a resource is expressed in JSON format, unassigned attributes, although they are defined in
-     * schema, MAY be omitted for compactness
-     * @param prefix
-     * @param map
-     * @param destination
-     * @param include
-     */
-    private void traverse(String prefix, Map<String, Object> map, LinkedHashMap<String, Object> destination, SortedSet<String> include){
-
-        for (String key : map.keySet()){
-            Object value=map.get(key);
-            log.debug("key {}", key);
-            if (value!=null && containsProperty(include, prefix, key)){
-                log.debug("true");
-                if (value instanceof Map)
-                    value = smallerMap(getNewPrefix(prefix, key), (Map<String, Object>) value, include);
-                else
-                if (IntrospectUtil.isCollection(value.getClass())){
-                    List list=new ArrayList();
-                    Map<String, Object> innerMap;
-
-                    for (Object item : (Collection) value){
-                        if (item!=null)
-                            if (item instanceof Map) {
-                                innerMap=smallerMap(getNewPrefix(prefix, key), (Map<String, Object>) item, include);
-                                if (innerMap!=null)
-                                    list.add(innerMap);
-                            }
-                            else {
-                                list.add(item);
-                            }
-                    }
-                    value=list;
-                }
-                if (value!=null)
-                    destination.put(key, value);
-            }
-        }
-
-    }
-
-    String serializeToJson(BaseScimResource resource, String attributes, String exclussions) throws Exception{
-
-        SortedSet<String> include =new TreeSet<String>();
-        Class<? extends BaseScimResource> resourceClass=resource.getClass();
-        buildIncludeSet(include, resourceClass, resource.getSchemas(), attributes, exclussions);
-        log.debug("incl {}", include);
-        //Do generic serialization. This works for any POJO (not only subclasses of BaseScimResource)
-        Map<String, Object> map = mapper.convertValue(resource, Map.class);
-        //Using LinkedHashMap allows recursive routines to visit submaps in the same order as fields appear in java classes
-        LinkedHashMap<String, Object> newMap=new LinkedHashMap<String, Object>();
-        traverse("", map, newMap, include);
-
-        return mapper.writeValueAsString(newMap);
-    }
-
-    String serializeToJson(BaseScimResource resource) throws Exception{
-        return serializeToJson(resource, null, null);
+    public String getEndpointUrl() {
+        return endpointUrl;
     }
 
     public Response getErrorResponse(Response.Status status, String detail) {
@@ -213,16 +47,11 @@ public class BaseScimWebService {
     public Response getErrorResponse(int statusCode, ErrorScimType scimType, String detail) {
 
         ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setSchemas(Collections.singletonList(ERROR_RESPONSE_URI));
         errorResponse.setStatus(String.valueOf(statusCode));
         errorResponse.setScimType(scimType);
         errorResponse.setDetail(detail);
 
         return Response.status(statusCode).entity(errorResponse).build();
-    }
-
-    public String getEndpointUrl() {
-        return endpointUrl;
     }
 
 }
