7caa1178923456ff16ba5e173c92c2f67ca825e5
Jose G <bonustrack310@gmail.com>
2017-10-02 08:39:55 -0500

    Add logic for handling custom attributes in create and update users methods

--- server/src/main/java/org/gluu/oxtrust/service/scim2/Scim2UserService.java
+++ server/src/main/java/org/gluu/oxtrust/service/scim2/Scim2UserService.java
@@ -16,9 +16,13 @@ import javax.ws.rs.NotFoundException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.gluu.oxtrust.ldap.service.IGroupService;
 import org.gluu.oxtrust.ldap.service.IPersonService;
+import org.gluu.oxtrust.ldap.service.MemberService;
 import org.gluu.oxtrust.model.GluuCustomPerson;
 import org.gluu.oxtrust.model.GluuGroup;
-import org.gluu.oxtrust.model.scim2.provider.ResourceType;
+import org.gluu.oxtrust.model.scim2.BaseScimResource;
+import org.gluu.oxtrust.model.scim2.annotations.Schema;
+import org.gluu.oxtrust.model.scim2.extensions.Extension;
+import org.gluu.oxtrust.model.scim2.extensions.ExtensionField;
 import org.gluu.oxtrust.model.scim2.user.*;
 import org.gluu.oxtrust.service.external.ExternalScimService;
 import org.gluu.oxtrust.util.ServiceUtil;
@@ -27,6 +31,7 @@ import org.gluu.site.ldap.exception.DuplicateEntryException;
 import org.joda.time.format.ISODateTimeFormat;
 import org.slf4j.Logger;
 import org.xdi.config.oxtrust.AppConfiguration;
+import org.xdi.ldap.model.GluuBoolean;
 import org.xdi.util.Pair;
 
 import static org.xdi.ldap.model.GluuBoolean.*;
@@ -49,8 +54,8 @@ public class Scim2UserService implements Serializable {
     @Inject
     private AppConfiguration appConfiguration;
 
-    //@Inject
-    //private MemberService memberService;
+    @Inject
+    private MemberService memberService;
 
     @Inject
     private IPersonService personService;
@@ -64,8 +69,8 @@ public class Scim2UserService implements Serializable {
     @Inject
     private ServiceUtil serviceUtil;
 
-    //@Inject
-    //private PatchUtil patchUtil;
+    @Inject
+    private ExtensionService extService;
 
     private void checkUidExistence(String uid) throws Exception{
         if (personService.getPersonByUid(uid) != null)
@@ -185,21 +190,64 @@ public class Scim2UserService implements Serializable {
         person.setAttribute("oxTrustRole", getComplexMultivaluedAsArray(res.getRoles()));
         person.setAttribute("oxTrustx509Certificate", getComplexMultivaluedAsArray(res.getX509Certificates()));
 
-        //Pairwise identifiers are not supplied here
+        //Pairwise identifiers are not supplied here...
 
-        //TODO: custom attributes in extension
+        transferExtendedAttributesToPerson(res, person);
     }
 
-    private void transferAttributesToUserResource(GluuCustomPerson person, UserResource res) {
+    /**
+     * Takes all extended attributes found in the SCIM resource and copies them to a GluuCustomPerson
+     * This method is called after validations take place (see associated decorator for User Service), so all inputs are
+     * OK and can go straight to LDAP with no runtime surprises
+     * @param resource A SCIM resource used as origin of data
+     * @param person a GluuCustomPerson used as destination
+     */
+    private void transferExtendedAttributesToPerson(BaseScimResource resource, GluuCustomPerson person){
+
+        try {
+            //Gets all the extended attributes for this resource
+            Map<String, Object> extendedAttrs= resource.getExtendedAttributes();
+            
+            //Iterates over all extensions this type of resource might have
+            for (Extension extension : extService.getResourceExtensions(resource.getClass())){
+                Object val=extendedAttrs.get(extension.getUrn());
+
+                if (val!=null) {
+                    //Obtains the attribute/value(s) pairs in the current extension
+                    Map<String, Object> attrsMap = (Map<String, Object>) val;
+
+                    for (String attribute : attrsMap.keySet()) {
+                        Object value = attrsMap.get(attribute);
+
+                        //Ignore if the attribute is unassigned in this resource: destination will not be changed in this regard
+                        if (value != null) {
+                            //Get properly formatted string representations for the value(s) associated to the attribute
+                            List<String> values=extService.getStringAttributeValues(extension.getFields().get(attribute), value);
+                            log.debug("transferExtendedAttributesToPerson. Setting attribute '{}' with values ", attribute, values.toString());
+                            person.setAttribute(attribute, values.toArray(new String[]{}));
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e){
+            log.error(e.getMessage(), e);
+        }
+
+    }
+
+    public void transferAttributesToUserResource(GluuCustomPerson person, UserResource res) {
 
         //Set values in order of appearance in BaseScimResource class
+        List<String> schemas=new ArrayList<String>();
+        schemas.add(res.getClass().getAnnotation(Schema.class).id());
+        res.setSchemas(schemas);    //Further this list is fed if custom attributes are found
+
         res.setId(person.getInum());
         res.setExternalId(person.getAttribute("oxTrustExternalId"));
-        //TODO: set schemas
-        res.setSchemas(null);
 
         Meta meta=new Meta();
-        meta.setResourceType(ResourceType.getType(res.getClass()));
+        meta.setResourceType(BaseScimResource.getType(res.getClass()));
         meta.setCreated(person.getAttribute("oxTrustMetaCreated"));
         meta.setLastModified(person.getAttribute("oxTrustMetaLastModified"));
         meta.setLocation(person.getAttribute("oxTrustMetaLocation"));
@@ -228,7 +276,8 @@ public class Scim2UserService implements Serializable {
         res.setLocale(person.getAttribute("locale"));
         res.setTimezone(person.getTimezone());
 
-        res.setActive(Boolean.valueOf(person.getAttribute("oxTrustActive")));
+        res.setActive(Boolean.valueOf(person.getAttribute("oxTrustActive"))
+                || GluuBoolean.getByValue(person.getAttribute("gluuStatus")).isBooleanValue());
         res.setPassword(person.getUserPassword());
 
         res.setEmails(getAttributeListValue(person, Email.class, "oxTrustEmail"));
@@ -261,7 +310,45 @@ public class Scim2UserService implements Serializable {
 
         res.setPairwiseIdentitifers(person.getOxPPID());
 
-        //TODO: custom attributes in extension
+        transferExtendedAttributesToResource(person, res);
+    }
+
+    private void transferExtendedAttributesToResource(GluuCustomPerson person, BaseScimResource resource){
+
+        //Gets the list of extensions associated to the resource passed. In practice, this will be at most a singleton list
+        List<Extension> extensions=extService.getResourceExtensions(resource.getClass());
+
+        //Iterate over every extension to copy extended attributes from person to resource
+        for (Extension extension : extensions){
+            Map<String, ExtensionField> fields=extension.getFields();
+            //Create empty map to store the values of the extended attributes found for current extension in object person
+            Map<String, Object> map=new HashMap<String, Object>();
+
+            log.debug("transferExtendedAttributesToResource. Revising attributes of extension '{}'", extension.getUrn());
+
+            //Iterate over every attribute part of this extension
+            for (String attr : fields.keySet()){
+                //Gets the values associated to this attribute that were found in LDAP
+                String values[]=person.getAttributes(attr);
+
+                if (values!=null){
+                    log.debug("transferExtendedAttributesToResource. Copying to resource the value(s) for attribute '{}'", attr);
+
+                    ExtensionField field=fields.get(attr);
+                    if (field.isMultiValued())
+                        map.put(attr, extService.convertValues(field, values));
+                    else
+                        map.put(attr, extService.convertValues(field, values).get(0));
+                }
+            }
+            //Stores all extended attributes (with their values) in the resource object
+            if (map.size()>0) {
+                resource.addExtendedAttributes(extension.getUrn(), map);
+            }
+        }
+        for (String urn : resource.getExtendedAttributes().keySet())
+            resource.getSchemas().add(urn);
+
     }
 
     private void assignComputedAttributesToPerson(GluuCustomPerson person){
@@ -300,7 +387,7 @@ public class Scim2UserService implements Serializable {
         log.info("Persisting user {}", userName);
         //TODO: uncomment addperson
         personService.addCustomObjectClass(gluuPerson);
-        //personService.addPerson(gluuPerson);
+        personService.addPerson(gluuPerson);
 
         user.getMeta().setLocation(location);
         user.setId(gluuPerson.getInum());
@@ -321,19 +408,32 @@ public class Scim2UserService implements Serializable {
             long now=new Date().getTime();
             tmpUser.getMeta().setLastModified(ISODateTimeFormat.dateTime().withZoneUTC().print(now));
 
-            tmpUser=(UserResource) ScimResourceUtil.transferToResource(user, tmpUser);
+            tmpUser=(UserResource) ScimResourceUtil.transferToResource(user, tmpUser, extService.getResourceExtensions(user.getClass()));
 
             transferAttributesToPerson(tmpUser, gluuPerson);
             gluuPerson.setCommonName(gluuPerson.getGivenName() + " " + gluuPerson.getSurname());
 
             personService.addCustomObjectClass(gluuPerson);
             //TODO: uncomment update person
-            //personService.updatePerson(gluuPerson);
+            personService.updatePerson(gluuPerson);
         }
         else{
             throw new NotFoundException("User resource with " + id + " not found");
         }
         return new Pair<GluuCustomPerson, UserResource>(gluuPerson, tmpUser);
+
+    }
+
+    public void deleteUser(GluuCustomPerson gluuPerson) throws Exception {
+
+        String dn = gluuPerson.getDn();
+        if (gluuPerson.getMemberOf()!= null && gluuPerson.getMemberOf().size()>0) {
+            log.info("Removing user {} from groups", gluuPerson.getUid());
+            serviceUtil.deleteUserFromGroup(gluuPerson, dn);
+        }
+        log.info("Removing user entry {}", dn);
+        //TODO: uncomment remove person
+        //memberService.removePerson(gluuPerson);
 
     }
 /*
