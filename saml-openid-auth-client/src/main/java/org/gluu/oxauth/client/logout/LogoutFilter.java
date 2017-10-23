def6c66b111becace3346e959a66c6943803bd65
Dmitry Ognyannikov <dmogn@mail.ru>
2017-10-23 02:08:52 -0300

    SAML LogoutFilter added

--- saml-openid-auth-client/src/main/java/org/gluu/oxauth/client/logout/LogoutFilter.java
+++ saml-openid-auth-client/src/main/java/org/gluu/oxauth/client/logout/LogoutFilter.java
@@ -6,42 +6,17 @@
 package org.gluu.oxauth.client.logout;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import net.shibboleth.idp.authn.ExternalAuthentication;
-import net.shibboleth.idp.profile.context.RelyingPartyContext;
-
-import org.codehaus.jettison.json.JSONObject;
 import org.gluu.oxauth.client.session.AbstractOAuthFilter;
-import org.gluu.oxauth.client.session.OAuthData;
 import org.gluu.oxauth.client.session.SignOutHandler;
-import org.gluu.oxauth.client.util.Configuration;
-import org.opensaml.profile.context.ProfileRequestContext;
-import org.xdi.oxauth.client.AuthorizationRequest;
-import org.xdi.oxauth.client.model.JwtState;
-import org.xdi.oxauth.model.common.ResponseType;
-import org.xdi.oxauth.model.crypto.OxAuthCryptoProvider;
-import org.xdi.oxauth.model.crypto.signature.SignatureAlgorithm;
-import org.xdi.oxauth.model.util.StringUtils;
-import org.xdi.util.ArrayHelper;
-import org.xdi.util.StringHelper;
-import org.xdi.util.security.StringEncrypter;
-import org.xdi.util.security.StringEncrypter.EncryptionException;
 
 /**
  * Filter implementation to intercept all requests and attempt to logout
