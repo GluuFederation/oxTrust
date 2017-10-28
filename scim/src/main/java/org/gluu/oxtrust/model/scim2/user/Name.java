984bc0debfdf0b241c7389cb6a21f750ab3b4b8d
Jose G <bonustrack310@gmail.com>
2017-10-28 11:23:19 -0500

    Replace LdapAttribute annotations

--- scim/src/main/java/org/gluu/oxtrust/model/scim2/user/Name.java
+++ scim/src/main/java/org/gluu/oxtrust/model/scim2/user/Name.java
@@ -2,62 +2,65 @@ package org.gluu.oxtrust.model.scim2.user;
 
 import org.apache.commons.lang.StringUtils;
 import org.gluu.oxtrust.model.scim2.annotations.Attribute;
-import org.gluu.site.ldap.persistence.annotation.LdapAttribute;
+import org.gluu.oxtrust.model.scim2.annotations.StoreReference;
 
 /**
  * Created by jgomer on 2017-09-04.
  *
  * The components of the user's full name.
- *
- * Do not remove LdapAttribute annotations. These are used by FilterVisitor classes to convert SCIM filter queries into LDAP queries
  */
 public class Name {
 
     @Attribute(description = "The family name of the User, or Last  Name in most Western languages (for example, Jensen " +
             "given the full name Ms. Barbara J Jensen, III.).")
-    @LdapAttribute(name = "sn")
+    @StoreReference(ref = "sn")
     private String familyName;
 
     @Attribute(description = "The given name of the User, or First Name  in most Western languages (for example, Barbara " +
             "given the full name Ms. Barbara J Jensen, III.).")
-    @LdapAttribute(name = "givenName")
+    @StoreReference(ref = "givenName")
     private String givenName;
 
     @Attribute(description = "The middle name(s) of the User (for example,  Robert given the full name Ms. Barbara J " +
             "Jensen, III.).")
-    @LdapAttribute(name = "middleName")
+    @StoreReference(ref = "middleName")
     private String middleName;
 
     @Attribute(description = "The honorific prefix(es) of the User, or  Title in most Western languages (for example, Ms. " +
             "given the full name Ms. Barbara J Jensen, III.).")
-    @LdapAttribute(name = "oxTrusthonorificPrefix")
+    @StoreReference(ref = "oxTrusthonorificPrefix")
     private String honorificPrefix;
 
     @Attribute(description = "The honorific suffix(es) of the User, or  Suffix in most Western languages (for example, " +
             "III. given the full name Ms. Barbara J Jensen, III.)")
-    @LdapAttribute(name = "oxTrusthonorificSuffix")
+    @StoreReference(ref = "oxTrusthonorificSuffix")
     private String honorificSuffix;
 
     @Attribute(description = "The full name, including all middle names, titles, and suffixes as appropriate, formatted " +
             "for display (for example, Ms. Barbara J Jensen, III.).")
     //This field is computed iff a value is not passed (ie is null)
+    @StoreReference(ref = "oxTrustNameFormatted")
     private String formatted;
 
-    public String getFormatted() {
+    public static String computeFormattedName(Name name){
 
-        if (StringUtils.isEmpty(formatted)) {
+        if (StringUtils.isEmpty(name.formatted)) {
             StringBuilder formattedName = new StringBuilder("");
 
-            formattedName.append(StringUtils.isEmpty(honorificPrefix) ? "" : honorificPrefix + " ");
-            formattedName.append(StringUtils.isEmpty(givenName) ? "" : givenName + " ");
-            formattedName.append(StringUtils.isEmpty(middleName) ? "" : middleName + " ");
-            formattedName.append(StringUtils.isEmpty(familyName) ? "" : familyName + " ");
-            formattedName.append(StringUtils.isEmpty(honorificSuffix) ? "" : honorificSuffix);
+            formattedName.append(StringUtils.isEmpty(name.honorificPrefix) ? "" : name.honorificPrefix + " ");
+            formattedName.append(StringUtils.isEmpty(name.givenName) ? "" : name.givenName + " ");
+            formattedName.append(StringUtils.isEmpty(name.middleName) ? "" : name.middleName + " ");
+            formattedName.append(StringUtils.isEmpty(name.familyName) ? "" : name.familyName + " ");
+            formattedName.append(StringUtils.isEmpty(name.honorificSuffix) ? "" : name.honorificSuffix);
 
-            formatted=formattedName.toString().trim();
+            name.formatted=formattedName.toString().trim();
         }
-        return formatted;
+        return name.formatted;
 
+    }
+
+    public String getFormatted() {
+        return formatted;
     }
 
     public void setFormatted(String formatted) {
