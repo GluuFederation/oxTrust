948b179240a75afe4a158b1e4107d7f9b5ce5a40
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2017-11-27 14:27:00 +0300

    Sync with master

--- server/src/main/java/org/gluu/oxtrust/action/UpdateOrganizationAction.java
+++ server/src/main/java/org/gluu/oxtrust/action/UpdateOrganizationAction.java
@@ -25,8 +25,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.gluu.jsf2.message.FacesMessages;
 import org.gluu.jsf2.model.RenderParameters;
 import org.gluu.jsf2.service.ConversationService;
