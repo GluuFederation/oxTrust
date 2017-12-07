637cecb71dbaf1cb140d597900c73cf663b584ba
Dmitry Ognyannikov <dmogn@mail.ru>
2017-12-07 20:29:01 -0300

    Prepare working prototype which demonstrates oxTrust API #784

---
+++ server/src/main/java/org/gluu/oxtrust/api/saml/TrustRelationshipWebService.java
@@ -0,0 +1,23 @@
+/*
+ * oxTrust is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2017, Gluu
+ */
+package org.gluu.oxtrust.api.saml;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * WS endpoint for TrustRelationship actions.
+ * 
+ * @author Dmitry Ognyannikov
+ */
+@Path("/saml/tr")
+public class TrustRelationshipWebService {
+    //TODO
+}
