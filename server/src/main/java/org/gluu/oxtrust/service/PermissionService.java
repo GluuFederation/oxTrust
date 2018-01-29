e009d210e90959894eb221b29ce9815a06dd8ec8
Dmitry Ognyannikov <dmogn@mail.ru>
2018-01-29 06:00:25 -0300

    license header added

--- server/src/main/java/org/gluu/oxtrust/service/PermissionService.java
+++ server/src/main/java/org/gluu/oxtrust/service/PermissionService.java
@@ -1,3 +1,8 @@
+/*
+ * oxTrust is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2017, Gluu
+ */
 package org.gluu.oxtrust.service;
 
 import org.gluu.oxtrust.ldap.service.ApplianceService;
