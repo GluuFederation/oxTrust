5a95758fc01d442447f4be2e51875d0d7e251246
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2018-03-06 21:24:20 +0300

    Make Sector Identifier GUID instead of INUM #809

--- server/src/main/java/org/gluu/oxtrust/util/OxTrustConstants.java
+++ server/src/main/java/org/gluu/oxtrust/util/OxTrustConstants.java
@@ -37,6 +37,7 @@ public final class OxTrustConstants extends OxConstants {
     public static final String origin = "gluuAttributeOrigin";
     public static final String mail = "mail";
     public static final String ppid = "oxPPID";
+    public static final String oxId = "oxId";
 
     public static final String status = "status";
     public static final String gluuStatus = "gluuStatus";
