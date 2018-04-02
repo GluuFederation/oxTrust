bca0377dccdd5912f77798dd13da1de8cee10c2b
Dmitry Ognyannikov <dmogn@mail.ru>
2018-04-02 03:28:08 -0300

    API client - log JSON request

--- client/src/main/java/org/gluu/oxtrust/api/client/util/ClientRequestLoggingFilter.java
+++ client/src/main/java/org/gluu/oxtrust/api/client/util/ClientRequestLoggingFilter.java
@@ -5,6 +5,7 @@
  */
 package org.gluu.oxtrust.api.client.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,15 +19,17 @@ import javax.ws.rs.client.ClientRequestFilter;
  */
 public class ClientRequestLoggingFilter implements ClientRequestFilter {
     private static final Logger logger = Logger.getLogger(ClientRequestLoggingFilter.class.getName());
+    
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void filter(ClientRequestContext requestContext) throws IOException {
         logger.log(Level.INFO, "Request URI: {0}", requestContext.getUri());
         logger.log(Level.INFO, "Request method: {0}", requestContext.getMethod());
-        if (requestContext == null || requestContext.getEntity() == null) {
+        if (requestContext.getEntity() == null) {
             logger.log(Level.INFO, "empty requestContext");
         }
         else
-            logger.log(Level.INFO, requestContext.getEntity().toString());
+            logger.log(Level.INFO, objectMapper.writeValueAsString(requestContext.getEntity()));
     }
 }
