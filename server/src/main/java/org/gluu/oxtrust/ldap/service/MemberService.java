fa88afd966bdfc9e6870055e5b8988e60b79012f
yurem <Yuriy.Movchan@gmail.com>
2018-04-09 07:46:13 +0300

    Delete group is not working #944

--- server/src/main/java/org/gluu/oxtrust/ldap/service/MemberService.java
+++ server/src/main/java/org/gluu/oxtrust/ldap/service/MemberService.java
@@ -1,6 +1,7 @@
 package org.gluu.oxtrust.ldap.service;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -8,21 +9,23 @@ import javax.inject.Named;
 
 import org.gluu.oxtrust.model.GluuCustomPerson;
 import org.gluu.oxtrust.model.GluuGroup;
+import org.gluu.site.ldap.persistence.exception.EntryPersistenceException;
+import org.slf4j.Logger;
 import org.xdi.util.StringHelper;
 
-import javax.ejb.Stateless;
-
-@Stateless
 @Named
 public class MemberService implements Serializable {
 
 	private static final long serialVersionUID = -3545641785714134933L;
-	
+
 	@Inject
 	private GroupService groupService;
-	
+
 	@Inject
 	private PersonService personService;
+
+	@Inject
+	private Logger log;
 
 	public void removePerson(GluuCustomPerson person) {
 		// TODO: Do we realy need to remove group if owner is removed?
@@ -32,9 +35,30 @@ public class MemberService implements Serializable {
 				groupService.removeGroup(group);
 			}
 		}
-
+		// Remove person from associated groups
+		removePersonFromGroups(person);
 		// Remove person
 		personService.removePerson(person);
+	}
+
+	private void removePersonFromGroups(GluuCustomPerson person) {
+		log.debug("Removing person from associated group before deletion");
+        String pesonDn = person.getDn();
+		// Remove person from associated groups
+		List<String> associatedGroupsDn = person.getMemberOf();
+		for (String groupDn : associatedGroupsDn) {
+			GluuGroup group = groupService.getGroupByDn(groupDn);
+
+			List<String> members = new ArrayList<String>(group.getMembers());
+			members.remove(pesonDn);
+			group.setMembers(members);
+			try {
+				groupService.updateGroup(group);
+			} catch (EntryPersistenceException ex) {
+			    log.error("Failed to remove preson '{}' from group '{}'", ex);
+			}
+		}
+		log.debug("All group updated");
 	}
 
 }
