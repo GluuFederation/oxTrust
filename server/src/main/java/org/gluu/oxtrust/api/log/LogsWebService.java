77cb4d168ecefe7bbecd4d42bf97345bfb984ef4
EArezki <arezki.elmehdi@gmail.com>
2018-06-04 19:15:15 +0000

    #785/add an endpoint to read a log file by id givig a number of files

--- server/src/main/java/org/gluu/oxtrust/api/log/LogsWebService.java
+++ server/src/main/java/org/gluu/oxtrust/api/log/LogsWebService.java
@@ -1,20 +1,17 @@
 package org.gluu.oxtrust.api.log;
 
-
 import com.wordnik.swagger.annotations.ApiOperation;
 import org.gluu.oxtrust.api.logs.LogFileApi;
+import org.gluu.oxtrust.api.logs.LogFileDefApi;
 import org.gluu.oxtrust.ldap.service.ApplianceService;
-import org.gluu.oxtrust.model.LogViewerConfig;
 import org.gluu.oxtrust.model.log.LogFiles;
 import org.gluu.oxtrust.util.OxTrustApiConstants;
 import org.slf4j.Logger;
 import org.xdi.service.JsonService;
 
+import javax.annotation.security.DeclareRoles;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -22,10 +19,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@Path(OxTrustApiConstants.BASE_API_URL + OxTrustApiConstants.LOGS)
+@Path(OxTrustApiConstants.BASE_API_URL + "/logs")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-// Secured ?
+@DeclareRoles({"access", "log"})
 public class LogsWebService {
 
     @Inject
@@ -40,11 +37,31 @@ public class LogsWebService {
     @ApiOperation(value = "Get all logs")
     public Response logs() {
         try {
-            LogFiles logFiles = new LogFiles();
-            LogViewerConfig config = logFiles.config(applianceService.getAppliance(), jsonService);
-            return Response.ok(toDto(logFiles.filesIndexedById(config))).build();
-        } catch (IOException e) {
+            LogFiles logFiles = new LogFiles(applianceService.getAppliance(), jsonService);
+            return Response.ok(toDto(logFiles.filesIndexedById())).build();
+        } catch (Exception e) {
             log.error("Error loading logs", e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    private List<LogFileDefApi> toDto(Map<Integer, String> filesById) {
+        List<LogFileDefApi> result = new ArrayList<LogFileDefApi>();
+        for (Map.Entry<Integer, String> fileById : filesById.entrySet()) {
+            result.add(new LogFileDefApi(fileById.getKey(), fileById.getValue()));
+        }
+        return result;
+    }
+
+    @GET
+    @ApiOperation(value = "Get #numberOfLines log by id")
+    @Path("/{id}/{numberOfLines}")
+    public Response log(@PathParam("id") int id, @PathParam("numberOfLines") @DefaultValue("400") int numberOfLines) {
+        try {
+            LogFiles logFiles = new LogFiles(applianceService.getAppliance(), jsonService);
+            return Response.ok(toDto(id, logFiles.logTailById(id, numberOfLines))).build();
+        } catch (IOException e) {
+            log.error("Error loading log", e);
             return Response.status(Response.Status.NOT_FOUND).build();
         } catch (Exception e) {
             log.error("Error loading logs", e);
@@ -52,12 +69,8 @@ public class LogsWebService {
         }
     }
 
-    private List<LogFileApi> toDto(Map<Integer, String> filesById) {
-        List<LogFileApi> result = new ArrayList<LogFileApi>();
-        for (Map.Entry<Integer, String> fileById : filesById.entrySet()) {
-            result.add(new LogFileApi(fileById.getKey(), fileById.getValue()));
-        }
-        return result;
+    private LogFileApi toDto(int id, String content) {
+        return new LogFileApi(id, content);
     }
 
 }
