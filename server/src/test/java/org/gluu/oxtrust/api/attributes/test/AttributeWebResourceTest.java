34ea632a8da0ae978a4d2495fc79af3359324b7f
yurem <Yuriy.Movchan@gmail.com>
2018-07-12 17:53:54 +0300

    Add get allAttributes Rest test

---
+++ server/src/test/java/org/gluu/oxtrust/api/attributes/test/AttributeWebResourceTest.java
@@ -0,0 +1,90 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package org.gluu.oxtrust.api.attributes.test;
+
+import static org.testng.Assert.*;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.client.Client;
+
+import org.gluu.oxtrust.api.GluuAttributeApi;
+import org.gluu.oxtrust.api.proxy.AttributeProxy;
+import org.gluu.oxtrust.util.OxTrustApiConstants;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.testng.Reporter;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.gluu.oxtrust.action.test.BaseTest;
+
+/**
+ * @author Yuriy Movchan
+ *
+ * @author Yuriy Movchan Date: 07/12/2018
+ */
+public class AttributeWebResourceTest extends BaseTest {
+
+    @ArquillianResource
+    private URI url;
+
+    /**
+     * Get all attributes via WebTarget
+     */
+    @RunAsClient
+    @Parameters({ "webTarget" })
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Test
+    public void loadAttributes1(@Optional @ArquillianResteasyResource("restv1") final WebTarget webTarget) {
+
+        Invocation.Builder builder= webTarget.path("/api/attributes").request(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+        List<GluuAttributeApi> allAttributes = builder.get(List.class);
+        
+        assertNotNull(allAttributes, "Failed to get all attributes");
+        assertTrue(allAttributes.size() > 0, "List with all attributes should be not empty");
+    }
+
+    /**
+     * Get all attributes via ResteasyWebTarget
+     */
+    @RunAsClient
+    @Parameters({ "webTarget" })
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Test
+    public void loadAttributes2(@Optional @ArquillianResteasyResource("restv1") final ResteasyWebTarget webTarget) {
+        AttributeProxy attributeProxy = webTarget.proxy(AttributeProxy.class);
+        List<GluuAttributeApi> allAttributes = attributeProxy.getAllAttributes();
+
+        assertNotNull(allAttributes, "Failed to get all attributes");
+        assertTrue(allAttributes.size() > 0, "List with all attributes should be not empty");
+    }
+
+    /**
+     * Get all attributes via Proxy
+     */
+    @RunAsClient
+    @Parameters({ "attributeProxy" })
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Test
+    public void loadAttributes3(@Optional @ArquillianResteasyResource("restv1") final AttributeProxy attributeProxy) {
+        List<GluuAttributeApi> allAttributes = attributeProxy.getAllAttributes();
+
+        assertNotNull(allAttributes, "Failed to get all attributes");
+        assertTrue(allAttributes.size() > 0, "List with all attributes should be not empty");
+    }
+
+}
--- server/src/test/java/org/gluu/oxtrust/service/TestResteasyInitializer.java
+++ server/src/test/java/org/gluu/oxtrust/service/TestResteasyInitializer.java
@@ -12,6 +12,7 @@ import java.util.Set;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.ext.Provider;
 
+import org.gluu.oxtrust.api.attributes.AttributeWebResource;
 import org.gluu.oxtrust.ws.rs.scim2.BulkWebService;
 import org.gluu.oxtrust.ws.rs.scim2.GroupWebService;
 import org.gluu.oxtrust.ws.rs.scim2.ResourceTypeWS;
@@ -25,6 +26,7 @@ import org.gluu.oxtrust.ws.rs.scim2.UserWebService;
  * @author Yuriy Movchan
  * @version June 6, 2017
  */
+// TODO: Try to move to test source folder
 @Provider
 public class TestResteasyInitializer extends Application {
 
@@ -37,6 +39,9 @@ public class TestResteasyInitializer extends Application {
         classes.add(ResourceTypeWS.class);
         classes.add(SchemaWebService.class);
         classes.add(ServiceProviderConfigWS.class);
+
+        // API
+        classes.add(AttributeWebResource.class);
 
         return classes;
     }
