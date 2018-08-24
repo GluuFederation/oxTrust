0ed42aabb41cd3b03949b987754d260bc638efea
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2018-08-24 15:28:09 +0300

    oxtrust : isAccessTokenAsJwt -> getAccessTokenAsJwt
    https://github.com/GluuFederation/oxTrust/issues/703
    https://github.com/GluuFederation/oxAuth/issues/638

--- server/src/main/java/org/gluu/oxtrust/model/OxAuthClient.java
+++ server/src/main/java/org/gluu/oxtrust/model/OxAuthClient.java
@@ -414,7 +414,7 @@ public class OxAuthClient extends Entry implements Serializable {
         this.subjectType = subjectType;
     }
 
-    public GluuBoolean isAccessTokenAsJwt() {
+    public GluuBoolean getAccessTokenAsJwt() {
         return accessTokenAsJwt;
     }
 
