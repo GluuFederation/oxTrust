7cf628f62287fb10a4b58ba3fcfaac7f8ea130bb
Shoeb Khan <shoebkhan09@gmail.com>
2018-12-07 10:54:21 +0530

    Issue #930: Added fixes and changes as per PR comments

--- server/src/main/java/org/gluu/oxtrust/api/customscripts/CustomScriptManagementService.java
+++ server/src/main/java/org/gluu/oxtrust/api/customscripts/CustomScriptManagementService.java
@@ -4,59 +4,60 @@
  * Copyright (c) 2014, Gluu
  */
 
-package org.gluu.oxtrust.api;
+package org.gluu.oxtrust.api.customscripts;
 
 import com.google.common.base.Optional;
+import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
 import org.gluu.oxtrust.exception.InvalidScriptDataException;
-import org.gluu.oxtrust.exception.ScriptNotFoundException;
 import org.gluu.oxtrust.ldap.service.ApplianceService;
 import org.gluu.oxtrust.ldap.service.OrganizationService;
-import org.gluu.oxtrust.model.CustomScriptDTO;
 import org.gluu.oxtrust.service.filter.ProtectedApi;
 import org.gluu.oxtrust.util.OxTrustApiConstants;
 import org.gluu.persist.exception.BasePersistenceException;
 import org.slf4j.Logger;
-import org.xdi.model.ScriptLocationType;
 import org.xdi.model.custom.script.CustomScriptType;
 import org.xdi.model.custom.script.model.CustomScript;
-import org.xdi.model.custom.script.model.ScriptError;
 import org.xdi.service.custom.script.AbstractCustomScriptService;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import static java.util.Collections.singletonList;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.apache.commons.lang.StringUtils.isNotEmpty;
 import static org.gluu.oxtrust.action.ManageCustomScriptAction.*;
-import static org.gluu.oxtrust.model.CustomScriptDTO.toDTO;
+import static org.gluu.oxtrust.api.customscripts.CustomScriptDTO.toDTO;
 
 /**
  * Service for custom script management
  *
  * @author Shoeb Khan
- * @version October 24, 2018
+ * @version November 20, 2018
  */
 
-
+@Api
 @Path(OxTrustApiConstants.BASE_API_URL + "/configurations/scripts")
 @ProtectedApi
+@Produces(APPLICATION_JSON)
+@Consumes(APPLICATION_JSON)
+
 public class CustomScriptManagementService {
 
     @Inject
@@ -69,14 +70,12 @@ public class CustomScriptManagementService {
     private ApplianceService applianceService;
 
     @Inject
+    private ScriptErrorResource errorResource;
+
+    @Inject
     private AbstractCustomScriptService customScriptService;
 
-    private static final String INVALID_SCRIPT_TYPE_ERROR = buildInvalidScriptTypeError();
-
-    private static final String INVALID_LOCATION_TYPE_ERROR = buildInvalidScriptLocationError();
-
-
-    private static final String SERVER_DENIED_THE_REQUEST = "access_denied\n" + "Server denied the request.";
+    static final String SERVER_DENIED_THE_REQUEST = "access_denied\n" + "Server denied the request.";
 
     /**
      * Returns all custom scripts by their types
@@ -84,7 +83,6 @@ public class CustomScriptManagementService {
      * @returns HTTP Response
      */
     @GET
-    @Produces({MediaType.APPLICATION_JSON})
     @ApiOperation(value = "Returns all custom scripts by their types")
     @ApiResponses({
             @ApiResponse(code = 200, message = "Found custom scripts. Returning all of them"),
@@ -110,8 +108,6 @@ public class CustomScriptManagementService {
      * @returns HTTP response
      */
     @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "Saves the script")
     @ApiResponses({
             @ApiResponse(code = 200, message = "Script saved."),
@@ -119,7 +115,7 @@ public class CustomScriptManagementService {
             @ApiResponse(code = 403, message =  SERVER_DENIED_THE_REQUEST),
             @ApiResponse(code = 500, message = "Problem saving custom script")
     })
-    public Response save(final CustomScriptDTO customScriptDTO) {
+    public Response save(@Valid final CustomScriptDTO customScriptDTO) {
 
         try {
 
@@ -151,7 +147,8 @@ public class CustomScriptManagementService {
      * @returns HTTP response
      */
     @DELETE
-    @Path("/inum/{inum}")
+    @Path("/{inum}")
+    @ApiOperation(value = "Deletes the script identified by inum")
     @ApiResponses({
             @ApiResponse(code = 200, message = "Deleted custom script"),
             @ApiResponse(code = 400, message = "Failed to find script for inum")
@@ -171,7 +168,7 @@ public class CustomScriptManagementService {
 
         }
 
-        throw new ScriptNotFoundException(String.format("Failed to find script for inum:%s", inum));
+        throw new NotFoundException(String.format("Failed to find script for inum:%s", inum));
 
     }
 
@@ -181,36 +178,15 @@ public class CustomScriptManagementService {
      * @returns HTTP response
      */
 
-    @GET
-    @Path("/errors/all")
-    @Produces({MediaType.APPLICATION_JSON})
-
+    @Path("/errors")
     @ApiOperation(value = "Returns all the scripts having errors. ")
     @ApiResponses({
             @ApiResponse(code = 200, message = "Found custom scripts with errors details."),
             @ApiResponse(code = 403, message =  SERVER_DENIED_THE_REQUEST),
             @ApiResponse(code = 500, message = "Failed to load custom scripts")
     })
-    public Response getAllScriptWithErrors() {
-
-        final List<CustomScript> allCustomScripts = customScriptService.findAllCustomScripts(
-                new String[] {"displayName","oxScriptError", "description"});
-
-        final List<CustomScriptDTO> scriptsWithError = new ArrayList<CustomScriptDTO>();
-
-        for (final Iterator<CustomScript> iterator = allCustomScripts.iterator(); iterator.hasNext(); ) {
-
-            final CustomScript customScript = iterator.next();
-
-            if (customScript.getScriptError() != null) {
-
-                final CustomScriptDTO scriptDTO = toDTO(customScript);
-                scriptsWithError.add(scriptDTO);
-
-            }
-        }
-
-        return Response.ok(scriptsWithError).build();
+    public ScriptErrorResource getAllScriptWithErrors() {
+        return errorResource;
     }
 
 
@@ -220,10 +196,7 @@ public class CustomScriptManagementService {
      * @param inum
      * @return
      */
-    @GET
-    @Path("/errors/inum/{inum}")
-    @Produces({MediaType.APPLICATION_JSON})
-
+    @Path("/errors")
     @ApiOperation(value = "Returns the error details of script,if any, for given INUM ")
     @ApiResponses({
             @ApiResponse(code = 200, message = "Found custom scripts with errors."),
@@ -231,25 +204,8 @@ public class CustomScriptManagementService {
             @ApiResponse(code = 500, message = "Script not found for inum.")
     })
 
-    public Response GetScriptErrorByINum(@PathParam(value = "inum") final String inum) {
-
-        final String baseDn = customScriptService.baseDn();
-
-        final Optional<CustomScript> scriptOpt =
-                customScriptService.getCustomScriptByINum(baseDn,inum, new String[]{"oxScriptError"});
-
-        if (! scriptOpt.isPresent()) {
-            throw new ScriptNotFoundException(String.format("Script not found for inum:%s", inum));
-        }
-
-        final CustomScript customScript =  scriptOpt.get();
-        final ScriptError scriptError = customScript.getScriptError();
-
-        if (scriptError == null) {
-            return Response.ok().build();
-        }
-
-        return Response.ok(scriptError).build();
+    public ScriptErrorResource GetScriptErrorByINum(@PathParam(value = "inum") final String inum) {
+        return errorResource;
     }
 
     private void validate(final CustomScriptDTO currentScript) {
@@ -260,11 +216,7 @@ public class CustomScriptManagementService {
 
             validateUsageType(currentScript);
 
-            validatePL(currentScript);
-
             validateScriptType(currentScript);
-
-            validateLocationType(currentScript);
 
         } catch (IllegalArgumentException ex) {
 
@@ -274,21 +226,12 @@ public class CustomScriptManagementService {
         }
     }
 
-    private static void validateLocationType(CustomScriptDTO currentScript) {
 
-        final String strLocationType = currentScript.getLocationType();
-
-        if (strLocationType == null ) {
-            throw new IllegalArgumentException("Location type is required.");
-        }
-
-        final ScriptLocationType locationType = ScriptLocationType.getByValue(currentScript.getLocationType());
-
-        if (locationType == null) {
-            throw new IllegalArgumentException(INVALID_LOCATION_TYPE_ERROR);
-        }
-    }
-
+    /**
+     * Additional behavior for verifying that, if script's type is PERSON_AUTHENTICATION,
+     * it's usage_type module property is set
+     *
+     */
     private void validateUsageType(CustomScriptDTO currentScript) {
         final CustomScriptType csType = CustomScriptType.getByValue(currentScript.getScriptType());
 
@@ -296,30 +239,15 @@ public class CustomScriptManagementService {
                 && currentScript.getUsageType() == null) throw new InvalidScriptDataException("Usage type is not specified");
     }
 
-    private void validatePL(final CustomScriptDTO scriptDTO) {
-
-        if (scriptDTO.getProgrammingLanguage() == null) {
-            throw new IllegalArgumentException("Script's programming language is required.");
-        }
-
-    }
-
     private void validateScriptType(final CustomScriptDTO scriptDTO) {
 
         final String strScriptType = scriptDTO.getScriptType();
 
-        if (strScriptType == null ) {
-            throw new IllegalArgumentException("Script type is required.");
-        }
-
         final CustomScriptType csType = CustomScriptType.getByValue(strScriptType);
 
-        if (csType == null) {
+           if (isNotEmpty(scriptDTO.getDn())) {
 
-            throw new IllegalArgumentException(INVALID_SCRIPT_TYPE_ERROR);
-        }
-
-        if (isNotEmpty(scriptDTO.getDn())) {
+            /* The script under action is NOT new. */
 
             final CustomScript existingScript = customScriptService.getCustomScriptByDn(scriptDTO.getDn(), "oxScriptType");
 
@@ -331,41 +259,6 @@ public class CustomScriptManagementService {
 
     }
 
-    private static String buildInvalidScriptTypeError() {
-
-        final StringBuilder sb = new StringBuilder("Value for script type is invalid. Supported script types are: ");
-
-        for (final CustomScriptType scriptType : CustomScriptType.values()) {
-            sb.append(scriptType.getDisplayName()).append(", ");
-        }
-
-        removeLastComma(sb);
-
-        return sb.toString();
-    }
-
-    private static String buildInvalidScriptLocationError() {
-
-        final StringBuilder sb = new StringBuilder("Value for script location is invalid. Supported locations are: ");
-
-        for (final ScriptLocationType locationType : ScriptLocationType.values()) {
-            sb.append(locationType.getDisplayName()).append(", ");
-        }
-
-        removeLastComma(sb);
-
-        return sb.toString();
-    }
-
-
-    private static void removeLastComma(StringBuilder sb) {
-
-        final int commaIdx = sb.lastIndexOf(",");
-
-        if (commaIdx > 0) {
-            sb.deleteCharAt(commaIdx);
-        }
-    }
 
     private static Map<CustomScriptType, List<CustomScriptDTO>> convertToDtoMap(final Map<CustomScriptType, List<CustomScript>> srcMap) {
 
