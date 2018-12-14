95baa3040417ff463115674352cb6cb7b7a3345a
Jose G <bonustrack310@gmail.com>
2018-12-14 10:53:42 -0500

    Adjustment for #1376

--- server/src/main/java/org/gluu/oxtrust/service/scim2/Scim2UserService.java
+++ server/src/main/java/org/gluu/oxtrust/service/scim2/Scim2UserService.java
@@ -121,7 +121,7 @@ public class Scim2UserService implements Serializable {
                 for (Object item : items)
                     itemList.add(mapper.writeValueAsString(item));
 
-                array = itemList.toArray(new String[]{});
+                array = itemList.toArray(new String[0]);
             }
         }
         catch (Exception e){
@@ -133,7 +133,7 @@ public class Scim2UserService implements Serializable {
 
     private <T> List<T> getAttributeListValue(GluuCustomPerson source, Class<T> clazz, String attrName) {
 
-        List<T> items = null;
+        List<T> items = new ArrayList<T>();
         try {
             ObjectMapper mapper = ServiceUtil.getObjectMapper();
             //This is already disabled in ServiceUtil
@@ -141,7 +141,6 @@ public class Scim2UserService implements Serializable {
 
             String[] attributeArray = source.getAttributeArray(attrName);
             if (attributeArray != null) {
-                items = new ArrayList<T>();
                 for (String attribute : attributeArray) {
                     T item = mapper.readValue(attribute, clazz);
                     items.add(item);
@@ -151,7 +150,7 @@ public class Scim2UserService implements Serializable {
         catch (Exception e){
             log.error(e.getMessage(), e);
         }
-        return items;
+        return items.size() == 0 ? null : items;
 
     }
 
@@ -386,14 +385,15 @@ public class Scim2UserService implements Serializable {
                 //Gets the values associated to this attribute that were found in LDAP
                 String values[]=person.getAttributes(attr);
 
-                if (values!=null){
-                    log.debug("transferExtendedAttributesToResource. Copying to resource the value(s) for attribute '{}'", attr);
+                if (values!=null) {
 
-                    ExtensionField field=fields.get(attr);
-                    if (field.isMultiValued())
-                        map.put(attr, extService.convertValues(field, values));
-                    else
-                        map.put(attr, extService.convertValues(field, values).get(0));
+                    log.debug("transferExtendedAttributesToResource. Copying to resource the value(s) for attribute '{}'", attr);
+                    ExtensionField field = fields.get(attr);
+                    List<Object> convertedValues = extService.convertValues(field, values);
+
+                    if (convertedValues.size() > 0) {
+                        map.put(attr, field.isMultiValued() ? convertedValues : convertedValues.get(0));
+                    }
                 }
             }
             //Stores all extended attributes (with their values) in the resource object
