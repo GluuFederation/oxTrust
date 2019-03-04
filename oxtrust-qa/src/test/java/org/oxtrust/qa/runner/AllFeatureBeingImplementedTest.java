ef7917bba010079fcacbce6075718c54d3c6734a
Gasmyr <thomas@gluu.org>
2019-03-04 13:20:17 +0100

    Fix typo in Qa test

--- oxtrust-qa/src/test/java/org/oxtrust/qa/runner/AllFeatureBeingImplementedTest.java
+++ oxtrust-qa/src/test/java/org/oxtrust/qa/runner/AllFeatureBeingImplementedTest.java
@@ -4,9 +4,9 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/java/org/oxtrust/qa/features" }, plugin = { "pretty",
-		"json:target/cucumber/json/cucumber.json" }, glue = "", monochrome = true, tags = { "@gluuQAPending" })
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features = { "src/test/java/org/oxtrust/qa/features" }, plugin = { "pretty",
+//		"json:target/cucumber/json/cucumber.json" }, glue = "", monochrome = true, tags = { "@gluuQAPending" })
 public class AllFeatureBeingImplementedTest {
 
 }
