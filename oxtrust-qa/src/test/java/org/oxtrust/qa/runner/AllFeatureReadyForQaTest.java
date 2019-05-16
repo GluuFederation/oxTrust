9886d2aa9475e2817e3215bdda216a3f0c0d319c
Gasmyr <thomas@gluu.org>
2019-05-16 13:43:13 +0100

    QA tool

--- oxtrust-qa/src/test/java/org/oxtrust/qa/runner/AllFeatureReadyForQaTest.java
+++ oxtrust-qa/src/test/java/org/oxtrust/qa/runner/AllFeatureReadyForQaTest.java
@@ -5,9 +5,9 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/java/org/oxtrust/qa/features" }, plugin = { "pretty",
-		"json:target/cucumber/json/cucumber.json" },glue="", monochrome = true, tags = { "@gluuQA" })
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features = { "src/test/java/org/oxtrust/qa/features" }, plugin = { "pretty",
+//		"json:target/cucumber/json/cucumber.json" },glue="", monochrome = true, tags = { "@gluuQA" })
 public class AllFeatureReadyForQaTest {
 
 }
