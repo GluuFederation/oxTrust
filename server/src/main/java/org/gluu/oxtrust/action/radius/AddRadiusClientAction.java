658f62cef6f25fe3e3c76852f22b373e1bc6ae70
Dzouato Djeumen Rolain Bonaventure <uprightech@gmail.com>
2019-06-27 06:00:40 +0100

    Changes in this commit:   * Added UI for Radius Client Management

---
+++ server/src/main/java/org/gluu/oxtrust/action/radius/AddRadiusClientAction.java
@@ -0,0 +1,76 @@
+package org.gluu.oxtrust.action.radius;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ConversationScoped;
+import javax.faces.application.FacesMessage;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.gluu.jsf2.message.FacesMessages;
+import org.gluu.oxtrust.ldap.service.EncryptionService;
+import org.gluu.oxtrust.ldap.service.radius.GluuRadiusClientService;
+import org.gluu.oxtrust.util.OxTrustConstants;
+import org.gluu.radius.model.RadiusClient;
+import org.gluu.service.security.Secure;
+import org.slf4j.Logger;
+
+@Named("addRadiusClientAction")
+@ConversationScoped
+@Secure("#{permissionService.hasPermission('radius','access')}")
+public class AddRadiusClientAction implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private static final Integer DEFAULT_PRIORITY = 1;
+
+    private RadiusClient client;
+
+    @Inject
+    private Logger log;
+
+    @Inject
+    private GluuRadiusClientService gluuRadiusClientService;
+
+    @Inject
+    private FacesMessages facesMessages;
+
+    @Inject
+    private EncryptionService encryptionService;
+
+    public RadiusClient getClient() {
+        return this.client;
+    }
+
+    public void setClient(RadiusClient client) {
+        this.client = client;
+    }
+
+    public void start() {
+
+        client  = new RadiusClient();
+        client.setPriority(DEFAULT_PRIORITY);
+    }
+
+    public String save() {
+
+        try {
+            String inum = gluuRadiusClientService.generateInum();
+            client.setInum(inum);
+            client.setDn(gluuRadiusClientService.getRadiusClientDn(inum));
+            String encsecret = encryptionService.encrypt(client.getSecret());
+            client.setSecret(encsecret);
+            gluuRadiusClientService.addRadiusClient(client);
+            facesMessages.add(FacesMessage.SEVERITY_INFO,"#{msg['radius.client.add.success']}");
+        }catch(Exception e) {
+            log.error("Could not add new radius client",e);
+            facesMessages.add(FacesMessage.SEVERITY_ERROR,"#{msg['radius.client.add.error']}");
+            return OxTrustConstants.RESULT_FAILURE;
+        }
+        return OxTrustConstants.RESULT_SUCCESS;
+    }
+
+    public String cancel() {
+
+        return OxTrustConstants.RESULT_SUCCESS;
+    }
+}
