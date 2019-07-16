d4159ec296bc43e6d099717e16ed6c7c0da8b5e0
Gasmyr <thomas@gluu.org>
2019-07-16 20:35:46 +0100

    QA-4.0 :- While try to add attribute on registration form getting Error Page. #1725

---
+++ server/src/main/java/org/gluu/oxtrust/action/AttributeNameConverter2.java
@@ -0,0 +1,37 @@
+package org.gluu.oxtrust.action;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+
+import org.gluu.model.GluuAttribute;
+import org.gluu.service.AttributeService;
+
+
+@FacesConverter("org.gluu.jsf2.converter.AttributeNameConverter2")
+public class AttributeNameConverter2 implements Converter {
+
+	@Inject
+	private AttributeService attributeService;
+	
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		if (value == null) {
+			return null;
+		}
+		return attributeService.getAttributeByName(value);
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		String string = null;
+		if (value instanceof GluuAttribute) {
+			string = ((GluuAttribute) value).getName();
+		}
+
+		return string;
+	}
+
+}
