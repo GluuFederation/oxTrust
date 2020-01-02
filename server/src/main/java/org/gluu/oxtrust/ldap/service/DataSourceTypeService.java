4fe3a031bf32b5f38589d2d7eace46882ec832c1
Gasmyr <thomas@gluu.org>
2020-01-02 13:40:52 +0100

    Unable to update log debug level for oxtrust from UI with couchbase backend #1898

--- server/src/main/java/org/gluu/oxtrust/ldap/service/DataSourceTypeService.java
+++ server/src/main/java/org/gluu/oxtrust/ldap/service/DataSourceTypeService.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.gluu.persist.PersistenceEntryManager;
+import org.gluu.persist.hybrid.impl.HybridEntryManagerFactory;
 import org.gluu.persist.ldap.impl.LdapEntryManagerFactory;
 
 @Stateless
@@ -16,11 +17,17 @@ public class DataSourceTypeService implements Serializable {
 	private static final long serialVersionUID = -1941135478226842653L;
 
 	@Inject
-	private PersistenceEntryManager ldapEntryManager;
+	private PersistenceEntryManager entryManager;
 
 	public boolean isLDAP() {
-		String result = ldapEntryManager.getPersistenceType(null);
-		if (result.equals(LdapEntryManagerFactory.PERSISTANCE_TYPE)) {
+		if (entryManager.getPersistenceType(null).equals(LdapEntryManagerFactory.PERSISTANCE_TYPE)) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isHyBird() {
+		if (entryManager.getPersistenceType(null).equals(HybridEntryManagerFactory.PERSISTANCE_TYPE)) {
 			return true;
 		}
 		return false;
