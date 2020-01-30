afb5cdc1acfba0c841bdbfdae4335799969cef84
Gasmyr <thomas@gluu.org>
2020-01-30 15:15:55 +0100

    Fix missing form validation on user add form

---
+++ server/src/main/java/org/gluu/oxtrust/util/GluuAttributeValidator.java
@@ -0,0 +1,25 @@
+package org.gluu.oxtrust.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.gluu.util.StringHelper;
+
+@FacesValidator("gluuAttributeValidator")
+public class GluuAttributeValidator implements Validator {
+	@Override
+	public void validate(FacesContext context, UIComponent comp, Object value) {
+		String attributeValue = (String) value;
+		if (StringHelper.isEmpty(attributeValue)) {
+			FacesMessage message = new FacesMessage("Value is required");
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(message);
+		}
+
+	}
+
+}
