d708999196ae81832fad24f436c2cfc39b867e42
Jose <bonustrack310@gmail.com>
2020-06-07 10:22:30 -0500

    Fix compilation issues

--- server/src/test/java/org/gluu/oxtrust/service/TestResteasyInitializer.java
+++ server/src/test/java/org/gluu/oxtrust/service/TestResteasyInitializer.java
@@ -1,9 +1,3 @@
-/*
- * oxTrust is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
- *
- * Copyright (c) 2014, Gluu
- */
-
 package org.gluu.oxtrust.service;
 
 import java.util.HashSet;
@@ -11,13 +5,6 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 import javax.ws.rs.ext.Provider;
-
-import org.gluu.oxtrust.ws.rs.scim2.BulkWebService;
-import org.gluu.oxtrust.ws.rs.scim2.GroupWebService;
-import org.gluu.oxtrust.ws.rs.scim2.ResourceTypeWS;
-import org.gluu.oxtrust.ws.rs.scim2.SchemaWebService;
-import org.gluu.oxtrust.ws.rs.scim2.ServiceProviderConfigWS;
-import org.gluu.oxtrust.ws.rs.scim2.UserWebService;
 
 /**
  * Integration with Resteasy
@@ -32,12 +19,6 @@ public class TestResteasyInitializer extends Application {
 	@Override
 	public Set<Class<?>> getClasses() {
 		HashSet<Class<?>> classes = new HashSet<Class<?>>();
-		classes.add(UserWebService.class);
-		classes.add(GroupWebService.class);
-		classes.add(BulkWebService.class);
-		classes.add(ResourceTypeWS.class);
-		classes.add(SchemaWebService.class);
-		classes.add(ServiceProviderConfigWS.class);
 		return classes;
 	}
 
