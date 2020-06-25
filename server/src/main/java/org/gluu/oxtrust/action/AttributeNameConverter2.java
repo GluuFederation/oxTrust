c2d00081a075841350c3bcedb35d2072bd04d99e
Gasmyr <thomas@gluu.org>
2020-06-25 13:13:31 +0100

    Configure Registration Form Attributes throws OOPS error #1982

--- server/src/main/java/org/gluu/oxtrust/action/AttributeNameConverter2.java
+++ server/src/main/java/org/gluu/oxtrust/action/AttributeNameConverter2.java
@@ -8,18 +8,21 @@ import javax.inject.Inject;
 
 import org.gluu.model.GluuAttribute;
 import org.gluu.service.AttributeService;
-
+import org.gluu.service.cdi.util.CdiUtil;
 
 @FacesConverter("org.gluu.jsf2.converter.AttributeNameConverter2")
 public class AttributeNameConverter2 implements Converter {
 
 	@Inject
 	private AttributeService attributeService;
-	
+
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component, String value) {
 		if (value == null) {
 			return null;
+		}
+		if (attributeService == null) {
+			attributeService = CdiUtil.bean(AttributeService.class);
 		}
 		return attributeService.getAttributeByName(value);
 	}
