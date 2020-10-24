7f3511765b31ee62fb2e228f6b88ab8921ed4ee1
Gasmyr <thomas@gluu.org>
2020-10-24 20:42:59 +0100

    Prevent registration of the attribute with the same name #2040

--- server/src/main/java/org/gluu/oxtrust/service/MetricService.java
+++ server/src/main/java/org/gluu/oxtrust/service/MetricService.java
@@ -36,227 +36,203 @@ import java.util.*;
 @Named(MetricService.METRIC_SERVICE_COMPONENT_NAME)
 public class MetricService extends org.gluu.service.metric.MetricService {
 
-	private static final int YEARLY = 365;
+    private static final int YEARLY = 365;
 
-	private static final long serialVersionUID = 7875838160379126796L;
+    private static final long serialVersionUID = 7875838160379126796L;
 
-	public static final String METRIC_SERVICE_COMPONENT_NAME = "metricService";
-	private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+    public static final String METRIC_SERVICE_COMPONENT_NAME = "metricService";
+    private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
-	@Inject
-	private Logger log;
+    @Inject
+    private Logger log;
 
-	@Inject
-	private Instance<MetricService> instance;
+    @Inject
+    private Instance<MetricService> instance;
 
-	@Inject
-	private CacheService cacheService;
+    @Inject
+    private CacheService cacheService;
 
-	@Inject
-	private ConfigurationService configurationService;
+    @Inject
+    private ConfigurationService configurationService;
 
-	@Inject
-	private OrganizationService organizationService;
+    @Inject
+    private OrganizationService organizationService;
 
-	@Inject
-	private AppConfiguration appConfiguration;
+    @Inject
+    private AppConfiguration appConfiguration;
 
-	@Inject
+    @Inject
     private NetworkService networkService;
 
-	@Inject
-	@Named(ApplicationFactory.PERSISTENCE_METRIC_ENTRY_MANAGER_NAME)
+    @Inject
+    @Named(ApplicationFactory.PERSISTENCE_METRIC_ENTRY_MANAGER_NAME)
     @ReportMetric
     private PersistenceEntryManager ldapEntryManager;
 
-	public void initTimer() {
-		initTimer(this.appConfiguration.getMetricReporterInterval(), this.appConfiguration.getMetricReporterKeepDataDays());
-	}
+    public void initTimer() {
+        initTimer(this.appConfiguration.getMetricReporterInterval(),
+                this.appConfiguration.getMetricReporterKeepDataDays());
+    }
 
-	@Override
-	public String baseDn() {
-		return String.format("ou=statistic,o=metric");
-	}
+    @Override
+    public String baseDn() {
+        return String.format("ou=statistic,o=metric");
+    }
 
-	@Override
-	public org.gluu.service.metric.MetricService getMetricServiceInstance() {
-		return instance.get();
-	}
+    @Override
+    public org.gluu.service.metric.MetricService getMetricServiceInstance() {
+        return instance.get();
+    }
 
-	public AuthenticationChartDto genereateAuthenticationChartDto(int countDays) {
-		String key = OxTrustConstants.CACHE_METRICS_KEY + "#home";
-		AuthenticationChartDto authenticationChartDto = (AuthenticationChartDto) cacheService
-				.get(OxTrustConstants.CACHE_METRICS_NAME, key);
-		if (authenticationChartDto != null) {
-			return authenticationChartDto;
-		}
+    public AuthenticationChartDto genereateAuthenticationChartDto(int countDays) {
+        String key = OxTrustConstants.CACHE_METRICS_KEY + "#home";
+        AuthenticationChartDto authenticationChartDto = (AuthenticationChartDto) cacheService
+                .get(OxTrustConstants.CACHE_METRICS_NAME, key);
+        if (authenticationChartDto != null) {
+            return authenticationChartDto;
+        }
+        Map<MetricType, List<? extends MetricEntry>> entries = findAuthenticationMetrics(ApplicationType.OX_AUTH,
+                -countDays);
+        Map<MetricType, List<? extends MetricEntry>> yearlyEntris = findAuthenticationMetrics(ApplicationType.OX_AUTH,
+                -YEARLY);
+        Map<String, Long> yearlySuccessStats = calculateCounterStatistics(YEARLY,
+                (List<CounterMetricEntry>) yearlyEntris.get(MetricType.OXAUTH_USER_AUTHENTICATION_SUCCESS));
+        Long yearlySuccessfullRequest = yearlySuccessStats.values().stream().mapToLong(Long::valueOf).sum();
+        Map<String, Long> yearlyFailureStats = calculateCounterStatistics(YEARLY,
+                (List<CounterMetricEntry>) yearlyEntris.get(MetricType.OXAUTH_USER_AUTHENTICATION_FAILURES));
+        Long yearlyFailsRequest = yearlyFailureStats.values().stream().mapToLong(Long::valueOf).sum();
+        String[] labels = new String[countDays];
+        Long[] values = new Long[countDays];
+        Map<String, Long> successStats = calculateCounterStatistics(countDays,
+                (List<CounterMetricEntry>) entries.get(MetricType.OXAUTH_USER_AUTHENTICATION_SUCCESS));
+        labels = successStats.keySet().toArray(labels);
+        values = successStats.values().toArray(values);
+        authenticationChartDto = new AuthenticationChartDto();
+        authenticationChartDto.setLabels(labels);
+        authenticationChartDto.setSuccess(values);
+        Map<String, Long> failureStats = calculateCounterStatistics(countDays,
+                (List<CounterMetricEntry>) entries.get(MetricType.OXAUTH_USER_AUTHENTICATION_FAILURES));
+        values = new Long[countDays];
+        authenticationChartDto.setFailure(failureStats.values().toArray(values));
+        authenticationChartDto.setYearlyRequest(yearlySuccessfullRequest + yearlyFailsRequest);
+        cacheService.put(key, authenticationChartDto);
+        return authenticationChartDto;
+    }
 
-		Map<MetricType, List<? extends MetricEntry>> entries = findAuthenticationMetrics(ApplicationType.OX_AUTH,
-				-countDays);
+    private Map<MetricType, List<? extends MetricEntry>> findAuthenticationMetrics(ApplicationType applicationType,
+            int countDays) {
+        List<MetricType> metricTypes = new ArrayList<MetricType>();
+        metricTypes.add(MetricType.OXAUTH_USER_AUTHENTICATION_FAILURES);
+        metricTypes.add(MetricType.OXAUTH_USER_AUTHENTICATION_SUCCESS);
+        Date endDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, countDays);
+        Date startDate = calendar.getTime();
+        return findMetricEntry(applicationType, metricTypes, startDate, endDate);
+    }
 
-		Map<MetricType, List<? extends MetricEntry>> yearlyEntris = findAuthenticationMetrics(ApplicationType.OX_AUTH,
-				-YEARLY);
-		Map<String, Long> yearlySuccessStats = calculateCounterStatistics(YEARLY,
-				(List<CounterMetricEntry>) yearlyEntris.get(MetricType.OXAUTH_USER_AUTHENTICATION_SUCCESS));
-		Long[] yearlyValues = new Long[YEARLY];
-		yearlyValues = yearlySuccessStats.values().toArray(yearlyValues);
-		Long yearlyRequest = 0L;
-		for (Long number : yearlyValues) {
-			yearlyRequest = yearlyRequest + number;
-		}
-		Map<String, Long> yearlyFailureStats = calculateCounterStatistics(YEARLY,
-				(List<CounterMetricEntry>) yearlyEntris.get(MetricType.OXAUTH_USER_AUTHENTICATION_FAILURES));
-		yearlyValues = new Long[YEARLY];
-		yearlyValues = yearlyFailureStats.values().toArray(yearlyValues);
-		for (Long number : yearlyValues) {
-			yearlyRequest = yearlyRequest + number;
-		}
+    private Map<String, Long> calculateCounterStatistics(int countDays, List<CounterMetricEntry> metrics) {
+        // Prepare map with all dates
+        Map<String, Long> stats = new TreeMap<String, Long>();
+        Calendar calendar = Calendar.getInstance();
+        for (int i = 0; i <= countDays; i++) {
+            String dateString = df.format(calendar.getTime());
+            stats.put(dateString, 0L);
+            calendar.add(Calendar.DATE, -1);
+        }
 
-		String[] labels = new String[countDays];
-		Map<String, Long> successStats = calculateCounterStatistics(countDays,
-				(List<CounterMetricEntry>) entries.get(MetricType.OXAUTH_USER_AUTHENTICATION_SUCCESS));
-		labels = successStats.keySet().toArray(labels);
+        if ((metrics == null) || (metrics.size() == 0)) {
+            return stats;
+        }
 
-		Long[] values = new Long[countDays];
-		values = successStats.values().toArray(values);
+        // Detect servers restart and readjust counts
+        // Server restart condition: previous entry
+        // CounterMetricEntry.CounterMetricEntry.count > current entry
+        // CounterMetricEntry.CounterMetricEntry.count
+        CounterMetricEntry prevMetric = null;
+        long prevDayCount = 0L;
+        long adjust = 0;
+        for (CounterMetricEntry metric : metrics) {
+            Date date = metric.getCreationDate();
+            calendar.setTime(date);
 
-		authenticationChartDto = new AuthenticationChartDto();
+            // Detect server restarts
+            if ((prevMetric != null)
+                    && (prevMetric.getMetricData().getCount() > metric.getMetricData().getCount() + adjust)) {
+                // Last count before server restart
+                long count = prevMetric.getMetricData().getCount();
 
-		authenticationChartDto.setLabels(labels);
-		authenticationChartDto.setSuccess(values);
+                // Change adjust value
+                adjust = count;
+            }
 
-		Map<String, Long> failureStats = calculateCounterStatistics(countDays,
-				(List<CounterMetricEntry>) entries.get(MetricType.OXAUTH_USER_AUTHENTICATION_FAILURES));
-		values = new Long[countDays];
-		values = failureStats.values().toArray(values);
-		authenticationChartDto.setFailure(values);
+            long count = metric.getMetricData().getCount();
+            metric.getMetricData().setCount(count + adjust);
 
-		authenticationChartDto.setYearlyRequest(yearlyRequest);
+            prevMetric = metric;
+        }
 
-		cacheService.put(key, authenticationChartDto);
+        // Iterate through ordered by MetricEntry.startDate list and just make value
+        // snapshot at the end of the day
+        int prevDay = -1;
+        prevMetric = null;
+        prevDayCount = 0L;
+        for (CounterMetricEntry metric : metrics) {
+            Date date = metric.getCreationDate();
+            calendar.setTime(date);
 
-		return authenticationChartDto;
-	}
+            int currDay = calendar.get(Calendar.DAY_OF_MONTH);
+            if ((prevMetric != null) && (prevDay != currDay)) {
+                long count = prevMetric.getMetricData().getCount();
+                String dateString = df.format(prevMetric.getCreationDate());
+                stats.put(dateString, count - prevDayCount);
 
-	private Map<MetricType, List<? extends MetricEntry>> findAuthenticationMetrics(ApplicationType applicationType,
-			int countDays) {
-		List<MetricType> metricTypes = new ArrayList<MetricType>();
-		metricTypes.add(MetricType.OXAUTH_USER_AUTHENTICATION_FAILURES);
-		metricTypes.add(MetricType.OXAUTH_USER_AUTHENTICATION_SUCCESS);
+                // Show only difference, not total
+                prevDayCount = count;
+            }
 
-		Date endDate = new Date();
-		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.DATE, countDays);
+            prevMetric = metric;
+            prevDay = currDay;
+        }
 
-		Date startDate = calendar.getTime();
+        // Add last day statistic
+        long count = prevMetric.getMetricData().getCount();
+        String dateString = df.format(prevMetric.getCreationDate());
+        stats.put(dateString, count - prevDayCount);
 
-		Map<MetricType, List<? extends MetricEntry>> entries = findMetricEntry(applicationType, metricTypes, startDate, endDate);
+        return stats;
+    }
 
-		return entries;
-	}
+    @Override
+    public boolean isMetricReporterEnabled() {
+        if (this.appConfiguration.getMetricReporterEnabled() == null) {
+            return false;
+        }
 
-	private Map<String, Long> calculateCounterStatistics(int countDays, List<CounterMetricEntry> metrics) {
-		// Prepare map with all dates
-		Map<String, Long> stats = new TreeMap<String, Long>();
-		Calendar calendar = Calendar.getInstance();
-		for (int i = 0; i <= countDays; i++) {
-			String dateString = df.format(calendar.getTime());
-			stats.put(dateString, 0L);
-			calendar.add(Calendar.DATE, -1);
-		}
+        return this.appConfiguration.getMetricReporterEnabled();
+    }
 
-		if ((metrics == null) || (metrics.size() == 0)) {
-			return stats;
-		}
+    @Override
+    public ApplicationType getApplicationType() {
+        return ApplicationType.OX_TRUST;
+    }
 
-		// Detect servers restart and readjust counts
-		// Server restart condition: previous entry
-		// CounterMetricEntry.CounterMetricEntry.count > current entry
-		// CounterMetricEntry.CounterMetricEntry.count
-		CounterMetricEntry prevMetric = null;
-		long prevDayCount = 0L;
-		long adjust = 0;
-		for (CounterMetricEntry metric : metrics) {
-			Date date = metric.getCreationDate();
-			calendar.setTime(date);
-
-			// Detect server restarts
-			if ((prevMetric != null)
-					&& (prevMetric.getMetricData().getCount() > metric.getMetricData().getCount() + adjust)) {
-				// Last count before server restart
-				long count = prevMetric.getMetricData().getCount();
-
-				// Change adjust value
-				adjust = count;
-			}
-
-			long count = metric.getMetricData().getCount();
-			metric.getMetricData().setCount(count + adjust);
-
-			prevMetric = metric;
-		}
-
-		// Iterate through ordered by MetricEntry.startDate list and just make value
-		// snapshot at the end of the day
-		int prevDay = -1;
-		prevMetric = null;
-		prevDayCount = 0L;
-		for (CounterMetricEntry metric : metrics) {
-			Date date = metric.getCreationDate();
-			calendar.setTime(date);
-
-			int currDay = calendar.get(Calendar.DAY_OF_MONTH);
-			if ((prevMetric != null) && (prevDay != currDay)) {
-				long count = prevMetric.getMetricData().getCount();
-				String dateString = df.format(prevMetric.getCreationDate());
-				stats.put(dateString, count - prevDayCount);
-
-				// Show only difference, not total
-				prevDayCount = count;
-			}
-
-			prevMetric = metric;
-			prevDay = currDay;
-		}
-
-		// Add last day statistic
-		long count = prevMetric.getMetricData().getCount();
-		String dateString = df.format(prevMetric.getCreationDate());
-		stats.put(dateString, count - prevDayCount);
-
-		return stats;
-	}
-
-	@Override
-	public boolean isMetricReporterEnabled() {
-		if (this.appConfiguration.getMetricReporterEnabled() == null) {
-			return false;
-		}
-
-		return this.appConfiguration.getMetricReporterEnabled();
-	}
-
-	@Override
-	public ApplicationType getApplicationType() {
-		return ApplicationType.OX_TRUST;
-	}
-
-	private void dump(List<CounterMetricEntry> metrics) {
-		for (CounterMetricEntry metric : metrics) {
-			Date date = metric.getCreationDate();
-			long count = metric.getMetricData().getCount();
-			System.out.println(date + " : " + count);
-		}
-	}
+    private void dump(List<CounterMetricEntry> metrics) {
+        for (CounterMetricEntry metric : metrics) {
+            Date date = metric.getCreationDate();
+            long count = metric.getMetricData().getCount();
+            System.out.println(date + " : " + count);
+        }
+    }
 
     @Override
     public PersistenceEntryManager getEntryManager() {
         return ldapEntryManager;
     }
 
-	@Override
-	public String getNodeIndetifier() {
-		return networkService.getMacAdress();
-	}
+    @Override
+    public String getNodeIndetifier() {
+        return networkService.getMacAdress();
+    }
 
 }
