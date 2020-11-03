61eafa30e0cf2fb4ae5e130b43b71d251c81d9c5
Gasmyr <thomas@gluu.org>
2020-11-03 18:52:49 +0100

    Fix missing parent metric branch  bug

--- service/src/main/java/org/gluu/oxtrust/service/ConfigurationService.java
+++ service/src/main/java/org/gluu/oxtrust/service/ConfigurationService.java
@@ -38,240 +38,248 @@ import java.util.Properties;
 @Named("configurationService")
 public class ConfigurationService implements Serializable {
 
-	private static final long serialVersionUID = 8842838732456296435L;
+    private static final long serialVersionUID = 8842838732456296435L;
 
-	@Inject
-	private Logger log;
+    @Inject
+    private Logger log;
 
-	@Inject
-	private PersistenceEntryManager persistenceEntryManager;
+    @Inject
+    private PersistenceEntryManager persistenceEntryManager;
 
-	@Inject
-	private OrganizationService organizationService;
+    @Inject
+    private OrganizationService organizationService;
 
-	@Inject
-	private EncryptionService encryptionService;
+    @Inject
+    private EncryptionService encryptionService;
 
-	private static final SimpleDateFormat PERIOD_DATE_FORMAT = new SimpleDateFormat("yyyyMM");
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final SimpleDateFormat PERIOD_DATE_FORMAT = new SimpleDateFormat("yyyyMM");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-	public boolean contains(String configurationDn) {
-		return persistenceEntryManager.contains(configurationDn, GluuConfiguration.class);
-	}
+    public boolean contains(String configurationDn) {
+        return persistenceEntryManager.contains(configurationDn, GluuConfiguration.class);
+    }
 
-	/**
-	 * Add new configuration
-	 * 
-	 * @param configuration
-	 *            Configuration
-	 */
-	public void addConfiguration(GluuConfiguration configuration) {
-		persistenceEntryManager.persist(configuration);
-	}
+    /**
+     * Add new configuration
+     * 
+     * @param configuration
+     *            Configuration
+     */
+    public void addConfiguration(GluuConfiguration configuration) {
+        persistenceEntryManager.persist(configuration);
+    }
 
-	/**
-	 * Update configuration entry
-	 * 
-	 * @param configuration
-	 *            GluuConfiguration
-	 */
-	public void updateConfiguration(GluuConfiguration configuration) {
-		try {
-			persistenceEntryManager.merge(configuration);
-		} catch (Exception e) {
-			log.info("", e);
-		}
-	}
+    /**
+     * Update configuration entry
+     * 
+     * @param configuration
+     *            GluuConfiguration
+     */
+    public void updateConfiguration(GluuConfiguration configuration) {
+        try {
+            persistenceEntryManager.merge(configuration);
+        } catch (Exception e) {
+            log.info("", e);
+        }
+    }
 
-	public void updateOxtrustStat(GluuOxTrustStat oxTrustStat) {
-		try {
-			persistenceEntryManager.merge(oxTrustStat);
-		} catch (Exception e) {
-			log.info("", e);
-		}
+    public void updateOxtrustStat(GluuOxTrustStat oxTrustStat) {
+        try {
+            persistenceEntryManager.merge(oxTrustStat);
+        } catch (Exception e) {
+            log.info("", e);
+        }
 
-	}
+    }
 
-	/**
-	 * Check if LDAP server contains configuration with specified attributes
-	 * 
-	 * @return True if configuration with specified attributes exist
-	 */
-	public boolean containsConfiguration(String dn) {
-		return persistenceEntryManager.contains(dn, GluuConfiguration.class);
-	}
+    /**
+     * Check if LDAP server contains configuration with specified attributes
+     * 
+     * @return True if configuration with specified attributes exist
+     */
+    public boolean containsConfiguration(String dn) {
+        return persistenceEntryManager.contains(dn, GluuConfiguration.class);
+    }
 
-	/**
-	 * Get configuration by inum
-	 * 
-	 * @param inum
-	 *            Configuration Inum
-	 * @return Configuration
-	 * @throws Exception
-	 */
-	public GluuConfiguration getConfigurationByInum(String inum) {
-		return persistenceEntryManager.find(GluuConfiguration.class, getDnForConfiguration());
-	}
+    /**
+     * Get configuration by inum
+     * 
+     * @param inum
+     *            Configuration Inum
+     * @return Configuration
+     * @throws Exception
+     */
+    public GluuConfiguration getConfigurationByInum(String inum) {
+        return persistenceEntryManager.find(GluuConfiguration.class, getDnForConfiguration());
+    }
 
-	/**
-	 * Get configuration
-	 * 
-	 * @return Configuration
-	 * @throws Exception
-	 */
-	public GluuConfiguration getConfiguration(String[] returnAttributes) {
-		GluuConfiguration result = null;
-		result = persistenceEntryManager.find(getDnForConfiguration(), GluuConfiguration.class, returnAttributes);
-		return result;
-	}
+    /**
+     * Get configuration
+     * 
+     * @return Configuration
+     * @throws Exception
+     */
+    public GluuConfiguration getConfiguration(String[] returnAttributes) {
+        GluuConfiguration result = null;
+        result = persistenceEntryManager.find(getDnForConfiguration(), GluuConfiguration.class, returnAttributes);
+        return result;
+    }
 
-	public GluuOxTrustStat getOxtrustStat(String[] returnAttributes) {
-		GluuOxTrustStat result = null;
-		if (persistenceEntryManager.contains(getDnForOxtrustStat(), GluuOxTrustStat.class)) {
-			result = persistenceEntryManager.find(getDnForOxtrustStat(), GluuOxTrustStat.class, returnAttributes);
-		} else {
-			result = new GluuOxTrustStat();
-			result.setDn(getDnForOxtrustStat());
-			persistenceEntryManager.persist(result);
-		}
-		return result;
-	}
+    public GluuOxTrustStat getOxtrustStat(String[] returnAttributes) {
+        GluuOxTrustStat result = new GluuOxTrustStat();
+        if (!persistenceEntryManager.contains(getDnForParentOxtrustStat(), GluuOxTrustStat.class)) {
+            result.setDn(getDnForParentOxtrustStat());
+            persistenceEntryManager.persist(result);
+        }
+        if (persistenceEntryManager.contains(getDnForOxtrustStat(), GluuOxTrustStat.class)) {
+            result = persistenceEntryManager.find(getDnForOxtrustStat(), GluuOxTrustStat.class, returnAttributes);
+        } else {
+            result.setDn(getDnForOxtrustStat());
+            persistenceEntryManager.persist(result);
+        }
+        return result;
+    }
 
-	/**
-	 * Get configuration
-	 * 
-	 * @return Configuration
-	 * @throws Exception
-	 */
-	public GluuConfiguration getConfiguration() {
-		return getConfiguration(null);
-	}
+    /**
+     * Get configuration
+     * 
+     * @return Configuration
+     * @throws Exception
+     */
+    public GluuConfiguration getConfiguration() {
+        return getConfiguration(null);
+    }
 
-	public GluuOxTrustStat getOxtrustStat() {
-		return getOxtrustStat(null);
-	}
+    public GluuOxTrustStat getOxtrustStat() {
+        return getOxtrustStat(null);
+    }
 
-	/**
-	 * Get all configurations
-	 * 
-	 * @return List of attributes
-	 * @throws Exception
-	 */
-	public List<GluuConfiguration> getConfigurations() {
-		return persistenceEntryManager.findEntries(getDnForConfiguration(), GluuConfiguration.class, null);
-	}
+    /**
+     * Get all configurations
+     * 
+     * @return List of attributes
+     * @throws Exception
+     */
+    public List<GluuConfiguration> getConfigurations() {
+        return persistenceEntryManager.findEntries(getDnForConfiguration(), GluuConfiguration.class, null);
+    }
 
-	/**
-	 * Build DN string for configuration
-	 * 
-	 * @return DN string for specified configuration or DN for configurations branch
-	 *         if inum is null
-	 * @throws Exception
-	 */
-	public String getDnForConfiguration() {
-		String baseDn = organizationService.getBaseDn();
-		return String.format("ou=configuration,%s", baseDn);
-	}
+    /**
+     * Build DN string for configuration
+     * 
+     * @return DN string for specified configuration or DN for configurations branch
+     *         if inum is null
+     * @throws Exception
+     */
+    public String getDnForConfiguration() {
+        String baseDn = organizationService.getBaseDn();
+        return String.format("ou=configuration,%s", baseDn);
+    }
 
-	public String getDnForOxtrustStat() {
-		return buildDn(LocalDateTime.now().format(formatter), new Date(), ApplicationType.OX_TRUST);
-	}
+    public String getDnForOxtrustStat() {
+        return buildDn(LocalDateTime.now().format(formatter), new Date(), ApplicationType.OX_TRUST);
+    }
 
-	public AuthenticationScriptUsageType[] getScriptUsageTypes() {
-		return new AuthenticationScriptUsageType[] { AuthenticationScriptUsageType.INTERACTIVE,
-				AuthenticationScriptUsageType.SERVICE, AuthenticationScriptUsageType.BOTH };
-	}
+    public String getDnForParentOxtrustStat() {
+        return buildDn(null, new Date(), ApplicationType.OX_TRUST);
+    }
 
-	public ProgrammingLanguage[] getProgrammingLanguages() {
-		return new ProgrammingLanguage[] { ProgrammingLanguage.PYTHON };
-	}
+    public AuthenticationScriptUsageType[] getScriptUsageTypes() {
+        return new AuthenticationScriptUsageType[] { AuthenticationScriptUsageType.INTERACTIVE,
+                AuthenticationScriptUsageType.SERVICE, AuthenticationScriptUsageType.BOTH };
+    }
 
-	public ScriptLocationType[] getLocationTypes() {
-		return new ScriptLocationType[] { ScriptLocationType.LDAP, ScriptLocationType.FILE };
-	}
+    public ProgrammingLanguage[] getProgrammingLanguages() {
+        return new ProgrammingLanguage[] { ProgrammingLanguage.PYTHON };
+    }
 
-	public CustomScriptType[] getCustomScriptTypes() {
-		return new CustomScriptType[] { CustomScriptType.PERSON_AUTHENTICATION, CustomScriptType.CONSENT_GATHERING,
-				CustomScriptType.UPDATE_USER, CustomScriptType.USER_REGISTRATION, CustomScriptType.CLIENT_REGISTRATION,
-				CustomScriptType.DYNAMIC_SCOPE, CustomScriptType.ID_GENERATOR, CustomScriptType.CACHE_REFRESH,
-				CustomScriptType.UMA_RPT_POLICY, CustomScriptType.UMA_CLAIMS_GATHERING, CustomScriptType.UMA_RPT_CLAIMS, CustomScriptType.INTROSPECTION,
-				CustomScriptType.RESOURCE_OWNER_PASSWORD_CREDENTIALS, CustomScriptType.APPLICATION_SESSION,
-				CustomScriptType.END_SESSION, CustomScriptType.SCIM, CustomScriptType.POST_AUTHN,
-				CustomScriptType.PERSISTENCE_EXTENSION, CustomScriptType.IDP };
-	}
+    public ScriptLocationType[] getLocationTypes() {
+        return new ScriptLocationType[] { ScriptLocationType.LDAP, ScriptLocationType.FILE };
+    }
 
-	public CustomScriptType[] getOthersCustomScriptTypes() {
-		return new CustomScriptType[] { CustomScriptType.CONSENT_GATHERING, CustomScriptType.UPDATE_USER,
-				CustomScriptType.USER_REGISTRATION, CustomScriptType.CLIENT_REGISTRATION,
-				CustomScriptType.DYNAMIC_SCOPE, CustomScriptType.ID_GENERATOR, CustomScriptType.CACHE_REFRESH,
-				CustomScriptType.UMA_RPT_POLICY, CustomScriptType.UMA_CLAIMS_GATHERING, CustomScriptType.UMA_RPT_CLAIMS, CustomScriptType.INTROSPECTION,
-				CustomScriptType.RESOURCE_OWNER_PASSWORD_CREDENTIALS, CustomScriptType.APPLICATION_SESSION,
-				CustomScriptType.END_SESSION, CustomScriptType.SCIM, CustomScriptType.POST_AUTHN,
-				CustomScriptType.PERSISTENCE_EXTENSION, CustomScriptType.IDP, CustomScriptType.CIBA_END_USER_NOTIFICATION};
-	}
+    public CustomScriptType[] getCustomScriptTypes() {
+        return new CustomScriptType[] { CustomScriptType.PERSON_AUTHENTICATION, CustomScriptType.CONSENT_GATHERING,
+                CustomScriptType.UPDATE_USER, CustomScriptType.USER_REGISTRATION, CustomScriptType.CLIENT_REGISTRATION,
+                CustomScriptType.DYNAMIC_SCOPE, CustomScriptType.ID_GENERATOR, CustomScriptType.CACHE_REFRESH,
+                CustomScriptType.UMA_RPT_POLICY, CustomScriptType.UMA_CLAIMS_GATHERING, CustomScriptType.UMA_RPT_CLAIMS,
+                CustomScriptType.INTROSPECTION, CustomScriptType.RESOURCE_OWNER_PASSWORD_CREDENTIALS,
+                CustomScriptType.APPLICATION_SESSION, CustomScriptType.END_SESSION, CustomScriptType.SCIM,
+                CustomScriptType.POST_AUTHN, CustomScriptType.PERSISTENCE_EXTENSION, CustomScriptType.IDP };
+    }
 
-	public void encryptedSmtpPassword(SmtpConfiguration smtpConfiguration) {
-		if (smtpConfiguration == null) {
-			return;
-		}
-		String password = smtpConfiguration.getPasswordDecrypted();
-		if (StringHelper.isNotEmpty(password)) {
-			try {
-				String encryptedPassword = encryptionService.encrypt(password);
-				smtpConfiguration.setPassword(encryptedPassword);
-			} catch (EncryptionException ex) {
-				log.error("Failed to encrypt SMTP password", ex);
-			}
-		}
-	}
+    public CustomScriptType[] getOthersCustomScriptTypes() {
+        return new CustomScriptType[] { CustomScriptType.CONSENT_GATHERING, CustomScriptType.UPDATE_USER,
+                CustomScriptType.USER_REGISTRATION, CustomScriptType.CLIENT_REGISTRATION,
+                CustomScriptType.DYNAMIC_SCOPE, CustomScriptType.ID_GENERATOR, CustomScriptType.CACHE_REFRESH,
+                CustomScriptType.UMA_RPT_POLICY, CustomScriptType.UMA_CLAIMS_GATHERING, CustomScriptType.UMA_RPT_CLAIMS,
+                CustomScriptType.INTROSPECTION, CustomScriptType.RESOURCE_OWNER_PASSWORD_CREDENTIALS,
+                CustomScriptType.APPLICATION_SESSION, CustomScriptType.END_SESSION, CustomScriptType.SCIM,
+                CustomScriptType.POST_AUTHN, CustomScriptType.PERSISTENCE_EXTENSION, CustomScriptType.IDP,
+                CustomScriptType.CIBA_END_USER_NOTIFICATION };
+    }
 
-	public void decryptSmtpPassword(SmtpConfiguration smtpConfiguration) {
-		if (smtpConfiguration == null) {
-			return;
-		}
-		String password = smtpConfiguration.getPassword();
-		if (StringHelper.isNotEmpty(password)) {
-			try {
-				smtpConfiguration.setPasswordDecrypted(encryptionService.decrypt(password));
-			} catch (EncryptionException ex) {
-				log.error("Failed to decrypt SMTP password", ex);
-			}
-		}
-	}
+    public void encryptedSmtpPassword(SmtpConfiguration smtpConfiguration) {
+        if (smtpConfiguration == null) {
+            return;
+        }
+        String password = smtpConfiguration.getPasswordDecrypted();
+        if (StringHelper.isNotEmpty(password)) {
+            try {
+                String encryptedPassword = encryptionService.encrypt(password);
+                smtpConfiguration.setPassword(encryptedPassword);
+            } catch (EncryptionException ex) {
+                log.error("Failed to encrypt SMTP password", ex);
+            }
+        }
+    }
 
-	public String getVersion() {
-		String version = getClass().getPackage().getImplementationVersion();
-		if (version == null) {
-			Properties prop = new Properties();
-			try (InputStream is = FacesContext.getCurrentInstance().getExternalContext()
-					.getResourceAsStream("/META-INF/MANIFEST.MF")) {
-				prop.load(is);
-				version = prop.getProperty("Implementation-Version");
-			} catch (IOException e) {
-				log.error(e.toString());
-			}
-		}
-		log.info("Starting App version " + version);
-		if (version != null) {
-			version = version.replace("-SNAPSHOT", "");
-			return version;
-		}
-		return "";
-	}
+    public void decryptSmtpPassword(SmtpConfiguration smtpConfiguration) {
+        if (smtpConfiguration == null) {
+            return;
+        }
+        String password = smtpConfiguration.getPassword();
+        if (StringHelper.isNotEmpty(password)) {
+            try {
+                smtpConfiguration.setPasswordDecrypted(encryptionService.decrypt(password));
+            } catch (EncryptionException ex) {
+                log.error("Failed to decrypt SMTP password", ex);
+            }
+        }
+    }
 
-	private String buildDn(String uniqueIdentifier, Date creationDate, ApplicationType applicationType) {
-		final StringBuilder dn = new StringBuilder();
-		if (StringHelper.isNotEmpty(uniqueIdentifier) && (creationDate != null) && (applicationType != null)) {
-			dn.append(String.format("uniqueIdentifier=%s,", uniqueIdentifier));
-		}
-		if ((creationDate != null) && (applicationType != null)) {
-			dn.append(String.format("ou=%s,", PERIOD_DATE_FORMAT.format(creationDate)));
-		}
-		if (applicationType != null) {
-			dn.append(String.format("ou=%s,", applicationType.getValue()));
-		}
-		dn.append("ou=statistic,o=metric");
-		return dn.toString();
-	}
+    public String getVersion() {
+        String version = getClass().getPackage().getImplementationVersion();
+        if (version == null) {
+            Properties prop = new Properties();
+            try (InputStream is = FacesContext.getCurrentInstance().getExternalContext()
+                    .getResourceAsStream("/META-INF/MANIFEST.MF")) {
+                prop.load(is);
+                version = prop.getProperty("Implementation-Version");
+            } catch (IOException e) {
+                log.error(e.toString());
+            }
+        }
+        log.info("Starting App version " + version);
+        if (version != null) {
+            version = version.replace("-SNAPSHOT", "");
+            return version;
+        }
+        return "";
+    }
+
+    private String buildDn(String uniqueIdentifier, Date creationDate, ApplicationType applicationType) {
+        final StringBuilder dn = new StringBuilder();
+        if (StringHelper.isNotEmpty(uniqueIdentifier) && (creationDate != null) && (applicationType != null)) {
+            dn.append(String.format("uniqueIdentifier=%s,", uniqueIdentifier));
+        }
+        if ((creationDate != null) && (applicationType != null)) {
+            dn.append(String.format("ou=%s,", PERIOD_DATE_FORMAT.format(creationDate)));
+        }
+        if (applicationType != null) {
+            dn.append(String.format("ou=%s,", applicationType.getValue()));
+        }
+        dn.append("ou=statistic,o=metric");
+        return dn.toString();
+    }
 
 }
