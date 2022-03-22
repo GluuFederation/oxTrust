ec07b33b610e8f3d0b4d76c8d935554567218793
yurem <Yuriy.Movchan@gmail.com>
2022-03-22 21:58:48 +0300

    fix: fir attribute selection

--- server/src/main/java/org/gluu/oxtrust/action/UserProfileAction.java
+++ server/src/main/java/org/gluu/oxtrust/action/UserProfileAction.java
@@ -273,7 +273,7 @@ public class UserProfileAction implements Serializable {
 			if (attribute.getName().equalsIgnoreCase(name)) {
 				List<String> values = Arrays.asList(attribute.getStringValues());
 				values.remove(value);
-				attribute.setValues(values);
+				attribute.setValues(values.toArray(new String[0]));
 				getPerson().setCustomAttributes(customAttributes);
 				if (values.isEmpty()) {
 					return true;
